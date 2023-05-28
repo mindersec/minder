36ecc549259c79291f518084546793f4538887a5
Luke Hinds <luke@stacklok.com>
2023-05-28 20:24:58 +0100

    gPRC mock
    Signed-off-by: Luke Hinds <luke@stacklok.com>

--- pkg/controlplane/handlers_organisation_test.go
+++ pkg/controlplane/handlers_organisation_test.go
@@ -1,7 +1,22 @@
+// Copyright 2023 Stacklok, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package controlplane
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
@@ -10,11 +25,13 @@ import (
 	"github.com/stacklok/mediator/pkg/db"
 	"github.com/stretchr/testify/assert"
 
+	"google.golang.org/grpc/codes"
+
 	mockdb "github.com/stacklok/mediator/database/mock"
 	pb "github.com/stacklok/mediator/pkg/generated/protobuf/go/mediator/v1"
 )
 
-func TestCreateOrganisation(t *testing.T) {
+func TestCreateOrganisationDBMock(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -58,4 +75,96 @@ func TestCreateOrganisation(t *testing.T) {
 	assert.Equal(t, expectedCreatedAt, response.CreatedAt.AsTime().In(time.UTC))
 	expectedUpdatedAt := expectedOrg.UpdatedAt.In(time.UTC)
 	assert.Equal(t, expectedUpdatedAt, response.UpdatedAt.AsTime().In(time.UTC))
+}
+
+func TestCreateOrganisation_gRPC(t *testing.T) {
+	testCases := []struct {
+		name               string
+		req                *pb.CreateOrganisationRequest
+		buildStubs         func(store *mockdb.MockStore)
+		checkResponse      func(t *testing.T, res *pb.CreateOrganisationResponse, err error)
+		expectedStatusCode codes.Code
+	}{
+		{
+			name: "Success",
+			req: &pb.CreateOrganisationRequest{
+				Name:    "TestOrg",
+				Company: "TestCompany",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateOrganisation(gomock.Any(), gomock.Any()).
+					Return(db.Organisation{
+						ID:        1,
+						Name:      "TestOrg",
+						Company:   "TestCompany",
+						CreatedAt: time.Now(),
+						UpdatedAt: time.Now(),
+					}, nil).
+					Times(1) // Add the Times(1) expectation to ensure it's called once
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateOrganisationResponse, err error) {
+				assert.NoError(t, err)
+				assert.NotNil(t, res)
+				assert.Equal(t, int32(1), res.Id)
+				assert.Equal(t, "TestOrg", res.Name)
+				assert.Equal(t, "TestCompany", res.Company)
+				assert.NotNil(t, res.CreatedAt)
+				assert.NotNil(t, res.UpdatedAt)
+			},
+			expectedStatusCode: codes.OK,
+		},
+		{
+			name: "EmptyRequest",
+			req: &pb.CreateOrganisationRequest{
+				Name:    "",
+				Company: "",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				// No expectations, as CreateOrganisation should not be called
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateOrganisationResponse, err error) {
+				// Assert the expected behavior when the request is empty
+				assert.Error(t, err)
+				assert.Nil(t, res)
+			},
+			expectedStatusCode: codes.InvalidArgument,
+		},
+		{
+			name: "StoreError",
+			req: &pb.CreateOrganisationRequest{
+				Name:    "TestOrg",
+				Company: "TestCompany",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateOrganisation(gomock.Any(), gomock.Any()).
+					Return(db.Organisation{}, errors.New("store error")).
+					Times(1)
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateOrganisationResponse, err error) {
+				// Assert the expected behavior when there's a store error
+				assert.Error(t, err)
+				assert.Nil(t, res)
+			},
+			expectedStatusCode: codes.Internal,
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			// Setup
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockStore := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(mockStore)
+
+			server := NewServer(mockStore)
+
+			resp, err := server.CreateOrganisation(context.Background(), tc.req)
+			tc.checkResponse(t, resp, err)
+		})
+	}
 }
