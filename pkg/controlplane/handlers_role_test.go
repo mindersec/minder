0847c3a32e235b30bf9260de5450135d653afbe9
Yolanda Robla <yolanda@stacklok.com>
2023-05-29 09:23:15 +0200

    fixes in tests

--- pkg/controlplane/handlers_role_test.go
+++ pkg/controlplane/handlers_role_test.go
@@ -16,44 +16,156 @@ package controlplane
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
+	"github.com/golang/mock/gomock"
+
+	"github.com/stacklok/mediator/pkg/db"
+	"github.com/stretchr/testify/assert"
+
+	"google.golang.org/grpc/codes"
+
+	mockdb "github.com/stacklok/mediator/database/mock"
 	pb "github.com/stacklok/mediator/pkg/generated/protobuf/go/mediator/v1"
-	"github.com/stacklok/mediator/pkg/util"
 )
 
-func TestRoleCreate(t *testing.T) {
-	seed := time.Now().UnixNano()
-	server := CreateTestServer()
-	if server == nil {
-		t.Fatalf("Failed to create server")
+func TestCreateRoleDBMock(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockStore := mockdb.NewMockStore(ctrl)
+
+	request := &pb.CreateRoleRequest{
+		GroupId: 1,
+		Name:    "TestRole",
 	}
 
-	// create organization and group first
-	org, err := server.CreateOrganisation(context.Background(), &pb.CreateOrganisationRequest{
-		Name:    util.RandomString(10, seed),
-		Company: util.RandomString(10, seed),
-	})
-
-	if err != nil {
-		t.Fatalf("Failed to create organisation: %v", err)
+	expectedRole := db.Role{
+		ID:          1,
+		GroupID:     1,
+		Name:        "TestOrg",
+		IsAdmin:     false,
+		IsProtected: false,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
 	}
 
-	group, err := server.CreateGroup(context.Background(), &pb.CreateGroupRequest{
-		OrganisationId: org.GetId(),
-		Name:           util.RandomString(10, seed),
-	})
+	mockStore.EXPECT().
+		CreateRole(gomock.Any(), gomock.Any()).
+		Return(expectedRole, nil)
 
-	role, err := server.CreateRole(context.Background(), &pb.CreateRoleRequest{
-		OrganisationId: org.Id,
-		GroupId:        group.GetGroupId(),
-		Name:           util.RandomString(10, seed),
-	})
-
-	if err != nil {
-		t.Fatalf("Failed to create role: %v", err)
+	server := &Server{
+		store: mockStore,
 	}
 
-	t.Logf("Created role: %v", role)
+	response, err := server.CreateRole(context.Background(), request)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+	assert.Equal(t, expectedRole.ID, response.Id)
+	assert.Equal(t, expectedRole.Name, response.Name)
+	assert.Equal(t, expectedRole.GroupID, response.GroupId)
+	assert.Equal(t, expectedRole.IsAdmin, response.IsAdmin)
+	assert.Equal(t, expectedRole.IsProtected, response.IsProtected)
+	expectedCreatedAt := expectedRole.CreatedAt.In(time.UTC)
+	assert.Equal(t, expectedCreatedAt, response.CreatedAt.AsTime().In(time.UTC))
+	expectedUpdatedAt := expectedRole.UpdatedAt.In(time.UTC)
+	assert.Equal(t, expectedUpdatedAt, response.UpdatedAt.AsTime().In(time.UTC))
+}
+
+func TestCreateRole_gRPC(t *testing.T) {
+	testCases := []struct {
+		name               string
+		req                *pb.CreateRoleRequest
+		buildStubs         func(store *mockdb.MockStore)
+		checkResponse      func(t *testing.T, res *pb.CreateRoleResponse, err error)
+		expectedStatusCode codes.Code
+	}{
+		{
+			name: "Success",
+			req: &pb.CreateRoleRequest{
+				GroupId: 1,
+				Name:    "TestRole",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateRole(gomock.Any(), gomock.Any()).
+					Return(db.Role{
+						ID:          1,
+						GroupID:     1,
+						Name:        "TestRole",
+						IsAdmin:     false,
+						IsProtected: false,
+						CreatedAt:   time.Now(),
+						UpdatedAt:   time.Now(),
+					}, nil).
+					Times(1)
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateRoleResponse, err error) {
+				assert.NoError(t, err)
+				assert.NotNil(t, res)
+				assert.Equal(t, int32(1), res.Id)
+				assert.Equal(t, "TestOrg", res.Name)
+				assert.Equal(t, 1, res.GroupId)
+				assert.Equal(t, false, res.IsAdmin)
+				assert.Equal(t, false, res.IsProtected)
+				assert.NotNil(t, res.CreatedAt)
+				assert.NotNil(t, res.UpdatedAt)
+			},
+			expectedStatusCode: codes.OK,
+		},
+		{
+			name: "EmptyRequest",
+			req: &pb.CreateRoleRequest{
+				Name: "",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				// No expectations, as CreateRole should not be called
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateRoleResponse, err error) {
+				// Assert the expected behavior when the request is empty
+				assert.Error(t, err)
+				assert.Nil(t, res)
+			},
+			expectedStatusCode: codes.InvalidArgument,
+		},
+		{
+			name: "StoreError",
+			req: &pb.CreateRoleRequest{
+				GroupId: 1,
+				Name:    "TestOrg",
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateRole(gomock.Any(), gomock.Any()).
+					Return(db.Role{}, errors.New("store error")).
+					Times(1)
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateRoleResponse, err error) {
+				// Assert the expected behavior when there's a store error
+				assert.Error(t, err)
+				assert.Nil(t, res)
+			},
+			expectedStatusCode: codes.Internal,
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockStore := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(mockStore)
+
+			server := NewServer(mockStore)
+
+			resp, err := server.CreateRole(context.Background(), tc.req)
+			tc.checkResponse(t, resp, err)
+		})
+	}
 }
