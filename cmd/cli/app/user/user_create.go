6a6e0cab3e223e8245bfb7784f78f48b8672ce76
Yolanda Robla Mota <yolanda@stacklok.com>
2023-06-13 16:19:40 +0200

    feat: allow to create users that do not belong to any org (#258)
    * feat: allow to create users that do not belong to any org
    
    Allow to do it by avoiding the "role-id" flag. In this case
    a question will arise asking the user if the desire is to
    create a self enrolled user.
    
    Closes: #237
    
    * create the role as admin by default

--- cmd/cli/app/user/user_create.go
+++ cmd/cli/app/user/user_create.go
@@ -22,10 +22,13 @@
 package user
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -33,6 +36,24 @@ import (
 	pb "github.com/stacklok/mediator/pkg/generated/protobuf/go/mediator/v1"
 	"github.com/stacklok/mediator/pkg/util"
 )
+
+// askForConfirmation asks the user for confirmation and returns true if confirmed, false otherwise.
+func askForConfirmation(prompt string) bool {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Printf("%s (y/n): ", prompt)
+	response, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response = strings.ToLower(strings.TrimSpace(response))
+	if response == "y" || response == "yes" {
+		return true
+	}
+
+	return false
+}
 
 // User_createCmd is the command for creating an user
 var User_createCmd = &cobra.Command{
@@ -47,17 +68,27 @@ within a mediator control plane.`,
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		// create the user via GRPC
-		role := util.GetConfigValue("role-id", "role-id", cmd, int32(0)).(int32)
+		role := viper.GetInt("role-id")
 		email := util.GetConfigValue("email", "email", cmd, nil)
 		username := util.GetConfigValue("username", "username", cmd, nil)
 		password := util.GetConfigValue("password", "password", cmd, nil)
 		firstName := util.GetConfigValue("firstname", "firstname", cmd, nil)
 		lastName := util.GetConfigValue("lastname", "lastname", cmd, nil)
 		isProtected := util.GetConfigValue("is-protected", "is-protected", cmd, false).(bool)
+		force := util.GetConfigValue("force", "force", cmd, false).(bool)
 
 		if username == nil {
 			fmt.Fprintf(os.Stderr, "Error: username is required\n")
 			os.Exit(1)
+		}
+
+		// if role is null, we need to ask if they want to create a single user
+		if role == 0 && !force {
+			confirmed := askForConfirmation("You didn't specify a role id. Do you want to create an user without any organization?")
+			if !confirmed {
+				fmt.Println("User creation cancelled")
+				os.Exit(0)
+			}
 		}
 
 		conn, err := util.GetGrpcConnection(cmd)
@@ -72,39 +103,79 @@ within a mediator control plane.`,
 			os.Exit(1)
 		}
 
-		client := pb.NewUserServiceClient(conn)
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 		defer cancel()
+
+		// now create the default fields for the user if needed
+		if role == 0 {
+			client := pb.NewOrganizationServiceClient(conn)
+			resp, err := client.CreateOrganization(ctx, &pb.CreateOrganizationRequest{
+				Name:    username.(string) + "-org",
+				Company: username.(string) + " - Self enrolled",
+			})
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error creating organization: %s\n", err)
+				os.Exit(1)
+			}
+
+			clientg := pb.NewGroupServiceClient(conn)
+			respg, err := clientg.CreateGroup(ctx, &pb.CreateGroupRequest{OrganizationId: resp.Id,
+				Name: username.(string) + "-group"})
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error creating group: %s\n", err)
+				os.Exit(1)
+			}
+
+			clientr := pb.NewRoleServiceClient(conn)
+			isAdmin := true
+			respr, err := clientr.CreateRole(ctx, &pb.CreateRoleRequest{GroupId: respg.GroupId,
+				Name: username.(string) + "-role", IsAdmin: &isAdmin})
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error creating role: %s\n", err)
+				os.Exit(1)
+			}
+
+			// now assign the role
+			role = int(respr.Id)
+		}
+
+		client := pb.NewUserServiceClient(conn)
 
 		protectedPtr := &isProtected
 
 		var emailPtr *string
-		if email == nil {
+		if email == nil || email == "" {
 			emailPtr = nil
 		} else {
-			emailPtr = email.(*string)
+			temp := email.(string)
+			emailPtr = &temp
 		}
 		var passwordPtr *string
-		if password == nil {
+		if password == nil || password == "" {
 			passwordPtr = nil
 		} else {
-			passwordPtr = password.(*string)
+			temp := password.(string)
+			passwordPtr = &temp
 		}
 		var firstNamePtr *string
-		if firstName == nil {
+		if firstName == nil || firstName == "" {
 			firstNamePtr = nil
 		} else {
-			firstNamePtr = password.(*string)
+			temp := firstName.(string)
+			firstNamePtr = &temp
 		}
 		var lastNamePtr *string
-		if lastName == nil {
+		if lastName == nil || lastName == "" {
 			lastNamePtr = nil
 		} else {
-			lastNamePtr = lastName.(*string)
+			temp := lastName.(string)
+			lastNamePtr = &temp
 		}
 
 		resp, err := client.CreateUser(ctx, &pb.CreateUserRequest{
-			RoleId:      role,
+			RoleId:      int32(role),
 			Email:       emailPtr,
 			Username:    username.(string),
 			Password:    passwordPtr,
@@ -135,12 +206,10 @@ func init() {
 	User_createCmd.Flags().StringP("firstname", "f", "", "User's first name")
 	User_createCmd.Flags().StringP("lastname", "l", "", "User's last name")
 	User_createCmd.Flags().BoolP("is-protected", "i", false, "Is the user protected")
-	User_createCmd.Flags().Int32P("role-id", "r", 0, "Role ID")
+	User_createCmd.Flags().Int32P("role-id", "r", 0, "Role ID. If empty, will create a single user")
+	User_createCmd.Flags().BoolP("force", "s", false, "Skip confirmation")
+
 	if err := User_createCmd.MarkFlagRequired("username"); err != nil {
-		fmt.Fprintf(os.Stderr, "Error marking flag as required: %s\n", err)
-		os.Exit(1)
-	}
-	if err := User_createCmd.MarkFlagRequired("role-id"); err != nil {
 		fmt.Fprintf(os.Stderr, "Error marking flag as required: %s\n", err)
 		os.Exit(1)
 	}
