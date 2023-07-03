f0616fc692ffab3c42f4eca56c5de37562622c45
Yolanda Robla Mota <info@ysoft.biz>
2023-07-03 12:29:20 +0200

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
   url: 'https://stacklok.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/mediator',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
