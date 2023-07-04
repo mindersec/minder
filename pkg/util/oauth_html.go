2ccb6cd99cbe12cf8b6090bec94be495bb881e16
Luke Hinds <luke@stacklok.com>
2023-07-04 08:16:08 +0100

    Add HTML to dedicated file
    Signed-off-by: Luke Hinds <luke@stacklok.com>

--- pkg/util/oauth_html.go
+++ pkg/util/oauth_html.go
@@ -1,25 +1,3 @@
-//
-// Copyright 2023 Stacklok, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// NOTE: This file is for stubbing out client code for proof of concept
-// purposes. It will / should be removed in the future.
-// Until then, it is not covered by unit tests and should not be used
-// It does make a good example of how to use the generated client code
-// for others to use as a reference.
-
-// Package util contains utility functions largely for unit testing.
 package util
 
 const (
