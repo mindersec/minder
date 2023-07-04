3a11c75745a02a9282009f3e728b735cb82d2c20
Luke Hinds <luke@stacklok.com>
2023-07-04 08:16:08 +0100

    Add HTML to dedicated file
    Signed-off-by: Luke Hinds <luke@stacklok.com>

--- pkg/controlplane/handlers_oauth.go
+++ pkg/controlplane/handlers_oauth.go
@@ -32,6 +32,7 @@ import (
 	"github.com/stacklok/mediator/pkg/db"
 	pb "github.com/stacklok/mediator/pkg/generated/protobuf/go/mediator/v1"
 	ghclient "github.com/stacklok/mediator/pkg/providers/github"
+	"github.com/stacklok/mediator/pkg/util"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 	"golang.org/x/oauth2"
@@ -39,35 +40,6 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-
-var html = `
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Mediator</title>
-		<style>
-			body {
-				background-color: #ffffff;
-				font-family: Arial, sans-serif;
-				text-align: center;
-				margin-top: 20vh;
-			}
-
-			h1 {
-				font-weight: bold;
-				font-size: 32px;
-				color: #000000;
-				margin-bottom: 20px;
-			}
-		</style>
-	</head>
-	<body>
-		<h1>Mediator enrollment complete</h1>
-		<p>You can now close this window and return to the CLI.</p>
-	</body>
-	</html>
-`
 
 func getDefaultGroup(ctx context.Context) (int32, error) {
 	claims, ok := ctx.Value(TokenInfoKey).(auth.UserClaims)
@@ -267,7 +239,7 @@ func (s *Server) ExchangeCodeForTokenCLI(ctx context.Context,
 
 	return &httpbody.HttpBody{
 		ContentType: "text/html",
-		Data:        []byte(html),
+		Data:        []byte(util.SessionHTML),
 	}, nil
 }
 
