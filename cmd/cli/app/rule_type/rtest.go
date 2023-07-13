f26708cda673f8e5a7edfbd7c45995d586dde7be
Juan Antonio Osorio <ozz@stacklok.com>
2023-07-13 11:08:43 +0300

    Add initial engine struct
    This adds a simple engine struct for rule types which does JSON schema validation.
    
    The intent is to add a full check of a rule against a certain entity.

--- cmd/cli/app/rule_type/rtest.go
+++ cmd/cli/app/rule_type/rtest.go
@@ -27,6 +27,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
+	"github.com/stacklok/mediator/internal/engine"
 	"github.com/stacklok/mediator/internal/util"
 	pb "github.com/stacklok/mediator/pkg/generated/protobuf/go/mediator/v1"
 	ghclient "github.com/stacklok/mediator/pkg/providers/github"
@@ -37,29 +38,78 @@ var testCmd = &cobra.Command{
 	Use:   "test",
 	Short: "test a rule type definition",
 	Long:  `The 'rule_type test' subcommand allows you test a rule type definition`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		fpath := cmd.Flag("file")
-		epath := cmd.Flag("entity")
+	RunE:  testCmdRun,
+}
 
-		rt, err := readRuleTypeFromFile(fpath.Value.String())
-		if err != nil {
-			return fmt.Errorf("error reading rule type from file: %w", err)
-		}
+func init() {
+	ruleTypeCmd.AddCommand(testCmd)
+	testCmd.Flags().StringP("rule-type", "r", "", "file to read rule type definition from")
+	testCmd.Flags().StringP("entity", "e", "", "YAML file containing the entity to test the rule against")
+	testCmd.Flags().StringP("fragment", "f", "", "YAML file containing a fragment of policy to test the rule against")
+	testCmd.Flags().StringP("token", "t", "", "token to authenticate to the provider."+
+		"Can also be set via the AUTH_TOKEN environment variable.")
 
-		fmt.Printf("Rule Type: %+v\n", rt)
+	if err := testCmd.MarkFlagRequired("rule-type"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error marking flag as required: %s\n", err)
+		os.Exit(1)
+	}
 
-		e, err = readEntityFromFile(epath.Value.String(), rt.Def.InEntity)
-		if err != nil {
-			return fmt.Errorf("error reading entity from file: %w", err)
-		}
+	if err := testCmd.MarkFlagRequired("entity"); err != nil {
+		fmt.Fprintf(os.Stderr, "Error marking flag as required: %s\n", err)
+		os.Exit(1)
+	}
 
-		_, err = getProviderClient(context.Background(), rt)
-		if err != nil {
-			return fmt.Errorf("error getting provider client: %w", err)
-		}
+	if err := viper.BindPFlag("auth.token", testCmd.Flags().Lookup("token")); err != nil {
+		fmt.Fprintf(os.Stderr, "Error binding flag: %s\n", err)
+		os.Exit(1)
+	}
+	// bind environment variable
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+}
 
-		return nil
-	},
+func testCmdRun(cmd *cobra.Command, _ []string) error {
+	rtpath := cmd.Flag("rule-type")
+	epath := cmd.Flag("entity")
+	fpath := cmd.Flag("fragment")
+
+	rt, err := readRuleTypeFromFile(rtpath.Value.String())
+	if err != nil {
+		return fmt.Errorf("error reading rule type from file: %w", err)
+	}
+
+	fmt.Printf("Rule Type: %+v\n", rt)
+
+	_, err = readEntityFromFile(epath.Value.String(), rt.Def.InEntity)
+	if err != nil {
+		return fmt.Errorf("error reading entity from file: %w", err)
+	}
+
+	frag, err := readFragmentFromFile(fpath.Value.String())
+	if err != nil {
+		return fmt.Errorf("error reading fragment from file: %w", err)
+	}
+
+	client, err := getProviderClient(context.Background(), rt)
+	if err != nil {
+		return fmt.Errorf("error getting provider client: %w", err)
+	}
+
+	eng, err := engine.NewRuleTypeEngine(rt, client)
+	if err != nil {
+		return fmt.Errorf("error creating rule type engine: %w", err)
+	}
+
+	valid, err := eng.ValidateAgainstSchema(frag)
+	if valid == nil {
+		return fmt.Errorf("error validating fragment against schema: %w", err)
+	}
+
+	fmt.Printf("Valid: %t\n", *valid)
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	}
+
+	return nil
 }
 
 func readRuleTypeFromFile(fpath string) (*pb.RuleType, error) {
@@ -114,6 +164,26 @@ func readEntityFromFile(fpath string, entType string) (any, error) {
 	return out, nil
 }
 
+func readFragmentFromFile(fpath string) (any, error) {
+	f, err := os.Open(filepath.Clean(fpath))
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %w", err)
+	}
+
+	w := &bytes.Buffer{}
+	if err := util.TranscodeYAMLToJSON(f, w); err != nil {
+		return nil, fmt.Errorf("error converting yaml to json: %w", err)
+	}
+
+	var out any
+
+	if err := json.NewDecoder(w).Decode(&out); err != nil {
+		return nil, fmt.Errorf("error decoding json: %w", err)
+	}
+
+	return out, nil
+}
+
 // getProviderClient returns a client for the provider specified in the rule type
 // definition.
 // TODO: This should be moved to a provider package and we should have some
@@ -127,29 +197,5 @@ func getProviderClient(ctx context.Context, rt *pb.RuleType) (ghclient.RestAPI, 
 		})
 	default:
 		return nil, fmt.Errorf("unknown provider: %s", rt.Context.Provider)
-	}
-}
-
-func init() {
-	ruleTypeCmd.AddCommand(testCmd)
-	testCmd.Flags().StringP("file", "f", "", "file to read rule type definition from")
-	if err := testCmd.MarkFlagRequired("file"); err != nil {
-		fmt.Fprintf(os.Stderr, "Error marking flag as required: %s\n", err)
-		os.Exit(1)
-	}
-
-	testCmd.Flags().StringP("token", "t", "", "token to authenticate to the provider."+
-		"Can also be set via the AUTH_TOKEN environment variable.")
-	if err := viper.BindPFlag("auth.token", testCmd.Flags().Lookup("token")); err != nil {
-		fmt.Fprintf(os.Stderr, "Error binding flag: %s\n", err)
-		os.Exit(1)
-	}
-	// bind environment variable
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-
-	testCmd.Flags().StringP("entity", "e", "", "YAML file containing the entity to test the rule against")
-	if err := testCmd.MarkFlagRequired("entity"); err != nil {
-		fmt.Fprintf(os.Stderr, "Error marking flag as required: %s\n", err)
-		os.Exit(1)
 	}
 }
