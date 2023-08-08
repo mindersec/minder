1e7755ecf9bd2501d3977dc85186dba5bb3fc8f1
Yolanda Robla Mota <yolanda@stacklok.com>
2023-08-08 11:58:31 +0200

    Update handlers_policy.go

--- pkg/controlplane/handlers_policy.go
+++ pkg/controlplane/handlers_policy.go
@@ -540,10 +540,6 @@ func (s *Server) CreateRuleType(ctx context.Context, crt *pb.CreateRuleTypeReque
 	}
 
 	entityCtx := engine.EntityFromContext(ctx)
-	fmt.Println("entity is")
-	fmt.Println(entityCtx)
-	fmt.Println(entityCtx.GetGroup().GetID())
-
 	_, err = s.store.GetRuleTypeByName(ctx, db.GetRuleTypeByNameParams{
 		Provider: entityCtx.GetProvider(),
 		GroupID:  entityCtx.GetGroup().GetID(),
