5922468095595013862348759a53ada67fa73498
Jakub Hrozek <jakub@stacklok.com>
2023-09-12 21:54:54 +0200

    Simplify and fix the code that fetches JS package info from the NPM registry
    The fixes include:
        - properly handle non-200 error codes
        - get rid of the separate npmPackageReply and packageJson
          structures, just parse what we need using a single struct
        - don't read the whole reply into a byte slice, use a reader instead

--- internal/engine/eval/vulncheck/pkgdb.go
+++ internal/engine/eval/vulncheck/pkgdb.go
@@ -19,7 +19,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 
 	pb "github.com/stacklok/mediator/pkg/generated/protobuf/go/mediator/v1"
@@ -35,10 +34,6 @@ type RepoQuerier interface {
 	SendRecvRequest(*http.Request) (patchFormatter, error)
 }
 
-type npmRepository struct {
-	endpoint string
-}
-
 func newRepository(ecoConfig *ecosystemConfig) (RepoQuerier, error) {
 	switch ecoConfig.Name {
 	case "npm":
@@ -49,116 +44,36 @@ func newRepository(ecoConfig *ecosystemConfig) (RepoQuerier, error) {
 	}
 }
 
-// todo(jakub): get rid of this and use jq to parse the json
 type packageJson struct {
-	Name       string `json:"name"`
-	Version    string `json:"version"`
-	Repository struct {
-		Type string `json:"type"`
-		URL  string `json:"url"`
-	} `json:"repository"`
-	Homepage                string   `json:"homepage"`
-	Description             string   `json:"description"`
-	Main                    string   `json:"main"`
-	Browser                 string   `json:"browser"`
-	Module                  string   `json:"module"`
-	Types                   string   `json:"types"`
-	DevDependencies         struct{} `json:"devDependencies"`
-	DevDependenciesComments struct {
-		Typescript string `json:"typescript"`
-	} `json:"devDependenciesComments"`
-	Author struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"author"`
-	Contributors []struct {
-		Name  string `json:"name"`
-		Email string `json:"email,omitempty"`
-		URL   string `json:"url"`
-	} `json:"contributors"`
-	Keywords []string `json:"keywords"`
-	Scripts  struct {
-		Start        string `json:"start"`
-		Lint         string `json:"lint"`
-		Build        string `json:"build"`
-		Test         string `json:"test"`
-		CheckTypes   string `json:"check-types"`
-		CypressOpen  string `json:"cypress:open"`
-		WebpackBuild string `json:"webpack-build"`
-	} `json:"scripts"`
-	Funding struct {
-		Type string `json:"type"`
-		URL  string `json:"url"`
-	} `json:"funding"`
-	Prettier struct {
-		PrintWidth    int    `json:"printWidth"`
-		Semi          bool   `json:"semi"`
-		SingleQuote   bool   `json:"singleQuote"`
-		QuoteProps    string `json:"quoteProps"`
-		TrailingComma string `json:"trailingComma"`
-	} `json:"prettier"`
-	Bugs struct {
-		URL string `json:"url"`
-	} `json:"bugs"`
-	License     string `json:"license"`
-	GitHead     string `json:"gitHead"`
-	ID          string `json:"_id"`
-	NodeVersion string `json:"_nodeVersion"`
-	NpmVersion  string `json:"_npmVersion"`
-	Dist        struct {
-		Integrity    string `json:"integrity"`
-		Shasum       string `json:"shasum"`
-		Tarball      string `json:"tarball"`
-		FileCount    int    `json:"fileCount"`
-		UnpackedSize int    `json:"unpackedSize"`
-		Signatures   []struct {
-			Keyid string `json:"keyid"`
-			Sig   string `json:"sig"`
-		} `json:"signatures"`
+	Name    string `json:"name"`
+	Version string `json:"version"`
+	Dist    struct {
+		Integrity string `json:"integrity"`
+		Tarball   string `json:"tarball"`
 	} `json:"dist"`
-	NpmUser struct {
-		Name  string `json:"name"`
-		Email string `json:"email"`
-	} `json:"_npmUser"`
-	Directories struct {
-	} `json:"directories"`
-	Maintainers []struct {
-		Name  string `json:"name"`
-		Email string `json:"email"`
-	} `json:"maintainers"`
-	NpmOperationalInternal struct {
-		Host string `json:"host"`
-		Tmp  string `json:"tmp"`
-	} `json:"_npmOperationalInternal"`
-	HasShrinkwrap bool `json:"_hasShrinkwrap"`
+}
+
+func (pj *packageJson) IndentedString(leadingWhitespace int) string {
+	padding := fmt.Sprintf("%*s", leadingWhitespace, "")
+	innerPadding := padding + "  " // Add 2 extra spaces
+
+	// format each line with leadingWhitespace and 2 extra spaces
+	data := padding + fmt.Sprintf("\"%s\": {\n", pj.Name)
+	data += innerPadding + fmt.Sprintf("\"version\": \"%s\",\n", pj.Version)
+	data += innerPadding + fmt.Sprintf("\"resolved\": \"%s\",\n", pj.Dist.Tarball)
+	data += innerPadding + fmt.Sprintf("\"integrity\": \"%s\",", pj.Dist.Integrity)
+
+	return data
+}
+
+type npmRepository struct {
+	endpoint string
 }
 
 func newNpmRepository(endpoint string) *npmRepository {
 	return &npmRepository{
 		endpoint: endpoint,
 	}
-}
-
-// TODO(jakub): fugly signature
-type npmPackageReply struct {
-	name      string
-	version   string
-	integrity string
-	tarball   string
-}
-
-func (r *npmPackageReply) IndentedString(leadingWhitespace int) string {
-	padding := fmt.Sprintf("%*s", leadingWhitespace, "")
-	innerPadding := padding + "  " // Add 2 extra spaces
-
-	// format each line with leadingWhitespace and 2 extra spaces
-	data := padding + fmt.Sprintf("\"%s\": {\n", r.name)
-	data += innerPadding + fmt.Sprintf("\"version\": \"%s\",\n", r.version)
-	data += innerPadding + fmt.Sprintf("\"resolved\": \"%s\",\n", r.tarball)
-	data += innerPadding + fmt.Sprintf("\"integrity\": \"%s\",", r.integrity)
-	// data += padding + "},"
-
-	return data
 }
 
 func (n npmRepository) NewRequest(ctx context.Context, dep *pb.Dependency) (*http.Request, error) {
@@ -179,21 +94,15 @@ func (_ npmRepository) SendRecvRequest(request *http.Request) (patchFormatter, e
 	}
 	defer resp.Body.Close()
 
-	content, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("could not read response: %w", err)
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("received non-200 response: %d", resp.StatusCode)
 	}
 
 	var pkgJson packageJson
-	err = json.Unmarshal(content, &pkgJson)
-	if err != nil {
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&pkgJson); err != nil {
 		return nil, fmt.Errorf("could not unmarshal response: %w", err)
 	}
 
-	return &npmPackageReply{
-		name:      pkgJson.Name,
-		version:   pkgJson.Version,
-		integrity: pkgJson.Dist.Integrity,
-		tarball:   pkgJson.Dist.Tarball,
-	}, nil
+	return &pkgJson, nil
 }
