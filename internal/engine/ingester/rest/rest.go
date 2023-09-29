351a3f39666daebbaaa9ee5dde002d1b96c4fc5f
Jakub Hrozek <jakub@stacklok.com>
2023-09-29 09:57:03 +0200

    util: Add assorted REST utils to reduce code duplication
    The REST remediator and the REST ingestor would share a bunch of code.
    Let's reduce the code duplication by providing several utility
    functions.

--- internal/engine/ingester/rest/rest.go
+++ internal/engine/ingester/rest/rest.go
@@ -31,6 +31,7 @@ import (
 
 	engif "github.com/stacklok/mediator/internal/engine/interfaces"
 	"github.com/stacklok/mediator/internal/providers"
+	"github.com/stacklok/mediator/internal/util"
 	pb "github.com/stacklok/mediator/pkg/api/protobuf/go/mediator/v1"
 	provifv1 "github.com/stacklok/mediator/pkg/providers/v1"
 )
@@ -57,16 +58,12 @@ func NewRestRuleDataIngest(
 		return nil, fmt.Errorf("missing endpoint")
 	}
 
-	tmpl := template.New("path")
-	tmpl, err := tmpl.Parse(restCfg.Endpoint)
+	tmpl, err := util.ParseNewTemplate(&restCfg.Endpoint, "endpoint")
 	if err != nil {
 		return nil, fmt.Errorf("cannot parse endpoint template: %w", err)
 	}
 
-	method := strings.ToUpper(restCfg.Method)
-	if len(method) == 0 {
-		method = http.MethodGet
-	}
+	method := util.HttpMethodFromString(restCfg.Method, http.MethodGet)
 
 	cli, err := pbuild.GetHTTP(context.Background())
 	if err != nil {
---
+++ internal/util/rest.go
@@ -0,0 +1,85 @@
+//
+// Copyright 2023 Stacklok, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package util provides helper functions for mediator
+package util
+
+import (
+	"bytes"
+	"fmt"
+	"net/url"
+	"strings"
+	"text/template"
+)
+
+// HttpMethodFromString returns the HTTP method from a string based on upprecase inMeth, defaulting to dfl
+func HttpMethodFromString(inMeth, dfl string) string {
+	method := strings.ToUpper(inMeth)
+	if len(method) == 0 {
+		method = dfl
+	}
+
+	return method
+}
+
+// ParseNewTemplate parses a named template from a string, ensuring it is not empty
+func ParseNewTemplate(tmpl *string, name string) (*template.Template, error) {
+	if tmpl == nil || len(*tmpl) == 0 {
+		return nil, fmt.Errorf("missing template")
+	}
+
+	t := template.New(name)
+	t, err := t.Parse(*tmpl)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse template: %w", err)
+	}
+
+	return t, nil
+}
+
+// GenerateCurlCommand generates a curl command from a method, apiBaseURL, endpoint, and body
+// this is useful to provide a dry-run for remediations
+func GenerateCurlCommand(method, apiBaseURL, endpoint, body string) (string, error) {
+	const tmplStr = `curl -L -X {{ .Method }} \
+ -H "Accept: application/vnd.github+json" \
+ -H "Authorization: Bearer $TOKEN" \
+ -H "X-GitHub-Api-Version: 2022-11-28" \
+ {{.URL}} \
+ -d '{{.Body}}'`
+
+	tmpl, err := template.New("curlCmd").Parse(tmplStr)
+	if err != nil {
+		return "", err
+	}
+
+	u, err := url.Parse(apiBaseURL)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse endpoint: %w", err)
+	}
+	u = u.JoinPath(endpoint)
+
+	var buf bytes.Buffer
+	data := map[string]string{
+		"Method": method,
+		"URL":    u.String(),
+		"Body":   body,
+	}
+
+	if err := tmpl.Execute(&buf, data); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
