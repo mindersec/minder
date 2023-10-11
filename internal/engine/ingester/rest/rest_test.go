dd8cc246ce62d37e1cf23fc1b9815c098e132567
Jakub Hrozek <jakub@stacklok.com>
2023-10-11 12:06:29 +0200

    ingestor: Return fallback body on certain HTTP status types
    At the moment, all HTTP errors from the ingestor are treated as errors
    and returned to the caller. This also means that neither the evaluator
    nor the remediate calls are called.
    
    Some endpoints, like branch protection, do not exist if branch
    protection is not enabled, which prevents us from going forward in the
    engine and actually remediating the issue. This is visible with the
    rule_type test command as:
    
    ```
    $ go run cmd/dev/main.go rule --profile /Users/jakub/devel/playground/remediate.yaml --rule-type examples/github/rule-types/branch_protection.yaml --entity /Users/jakub/devel/playground/repository.yaml
    Profile valid according to the JSON schema!
    Error: error evaluating rule type: error ingesting data: cannot do request: cannot make request: GET https://api.github.com/repos/jakubtestorg/testrepo/branches/main/protection: 404 Branch not protected []
    Error on execute: error evaluating rule type: error ingesting data:
    cannot do request: cannot make request: GET https://api.github.com/repos/jakubtestorg/testrepo/branches/main/protection: 404 Branch not protected []
    exit status 1
    ```
    
    This patch defines a configurable per-rule fallback where for rules that
    need this functionality, a dummy body is returned, which just falls
    throught the evaluator and then eventually makes the remediate step
    work.
    
    I chose this programmable fallback approach over just falling back to an
    empty body to prevent us from hiding issues where we might expand a
    template wrong or just have a typo in the endpoint - this way we will
    only proceed on error codes if the rule type explicitly says so.

--- internal/engine/ingester/rest/rest_test.go
+++ internal/engine/ingester/rest/rest_test.go
@@ -16,12 +16,19 @@
 package rest
 
 import (
+	"context"
 	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/reflect/protoreflect"
 
 	"github.com/stacklok/mediator/internal/db"
+	engif "github.com/stacklok/mediator/internal/engine/interfaces"
 	"github.com/stacklok/mediator/internal/providers"
 	pb "github.com/stacklok/mediator/pkg/api/protobuf/go/mediator/v1"
 	provifv1 "github.com/stacklok/mediator/pkg/providers/v1"
@@ -199,6 +206,229 @@ func TestNewRestRuleDataIngest(t *testing.T) {
 
 			require.NoError(t, err, "unexpected error")
 			require.NotNil(t, got, "expected non-nil")
+		})
+	}
+}
+
+func testGithubProviderBuilder(baseURL string) *providers.ProviderBuilder {
+	if !strings.HasSuffix(baseURL, "/") {
+		baseURL = baseURL + "/"
+	}
+
+	definitionJSON := `{
+		"github": {
+			"endpoint": "` + baseURL + `"
+		}
+	}`
+
+	return providers.NewProviderBuilder(
+		&db.Provider{
+			Name:       "github",
+			Version:    provifv1.V1,
+			Implements: []db.ProviderType{db.ProviderTypeGithub, db.ProviderTypeRest},
+			Definition: json.RawMessage(definitionJSON),
+		},
+		db.ProviderAccessToken{},
+		"token",
+	)
+}
+
+const (
+	validProtectionReply = `
+{
+  "url": "https://api.github.com/repos/jakubtestorg/testrepo/branches/main/protection",
+  "required_pull_request_reviews": {
+    "url": "https://api.github.com/repos/jakubtestorg/testrepo/branches/main/protection/required_pull_request_reviews",
+    "dismiss_stale_reviews": true,
+    "require_code_owner_reviews": true,
+    "require_last_push_approval": false,
+    "required_approving_review_count": 2
+  },
+  "required_signatures": {
+    "url": "https://api.github.com/repos/jakubtestorg/testrepo/branches/main/protection/required_signatures",
+    "enabled": false
+  },
+  "enforce_admins": {
+    "url": "https://api.github.com/repos/jakubtestorg/testrepo/branches/main/protection/enforce_admins",
+    "enabled": false
+  },
+  "required_linear_history": {
+    "enabled": false
+  },
+  "allow_force_pushes": {
+    "enabled": false
+  },
+  "allow_deletions": {
+    "enabled": false
+  },
+  "block_creations": {
+    "enabled": false
+  },
+  "required_conversation_resolution": {
+    "enabled": false
+  },
+  "lock_branch": {
+    "enabled": false
+  },
+  "allow_fork_syncing": {
+    "enabled": false
+  }
+}
+`
+	notFoundReply = `{"message": "Not Found"}`
+)
+
+func TestRestIngest(t *testing.T) {
+	t.Parallel()
+
+	type ingestArgs struct {
+		ent    protoreflect.ProtoMessage
+		params map[string]any
+	}
+
+	type newRestIngestArgs struct {
+		restCfg *pb.RestType
+		pbuild  *providers.ProviderBuilder
+	}
+
+	tests := []struct {
+		name        string
+		newIngArgs  newRestIngestArgs
+		ingArgs     ingestArgs
+		testHandler http.HandlerFunc
+		ingResultFn func() *engif.Result
+		wantErr     bool
+	}{
+		{
+			name: "valid ingest",
+			newIngArgs: newRestIngestArgs{
+				restCfg: &pb.RestType{
+					Endpoint: `/repos/{{.Entity.Owner}}/{{.Entity.Name}}/branches/{{ index .Params "branch" }}/protection`,
+					Parse:    "json",
+				},
+			},
+			ingArgs: ingestArgs{
+				ent: &pb.Repository{
+					Owner:  "OwnerVar",
+					Name:   "NameVar",
+					RepoId: 456,
+				},
+				params: map[string]any{
+					"branch": "main",
+				},
+			},
+			testHandler: func(writer http.ResponseWriter, request *http.Request) {
+				assert.Equal(t, "/repos/OwnerVar/NameVar/branches/main/protection", request.URL.Path, "unexpected path")
+				assert.Equal(t, http.MethodGet, request.Method, "unexpected method")
+
+				_, err := writer.Write([]byte(validProtectionReply))
+				assert.NoError(t, err, "unexpected error writing response")
+				writer.WriteHeader(http.StatusOK)
+			},
+			ingResultFn: func() *engif.Result {
+				var jReply any
+				if err := json.NewDecoder(strings.NewReader(validProtectionReply)).Decode(&jReply); err != nil {
+					return nil
+				}
+
+				return &engif.Result{
+					Object: jReply,
+				}
+			},
+			wantErr: false,
+		},
+		{
+			name: "test fallback",
+			newIngArgs: newRestIngestArgs{
+				restCfg: &pb.RestType{
+					Endpoint: `/repos/{{.Entity.Owner}}/{{.Entity.Name}}/branches/{{ index .Params "branch" }}/protection`,
+					Parse:    "json",
+					Fallback: []*pb.RestType_Fallback{
+						{
+							HttpCode: http.StatusNotFound,
+							Body:     notFoundReply,
+						},
+					},
+				},
+			},
+			ingArgs: ingestArgs{
+				ent: &pb.Repository{
+					Owner:  "OwnerVar",
+					Name:   "NameVar",
+					RepoId: 456,
+				},
+				params: map[string]any{
+					"branch": "main",
+				},
+			},
+			testHandler: func(writer http.ResponseWriter, request *http.Request) {
+				assert.Equal(t, "/repos/OwnerVar/NameVar/branches/main/protection", request.URL.Path, "unexpected path")
+				assert.Equal(t, http.MethodGet, request.Method, "unexpected method")
+
+				writer.WriteHeader(http.StatusNotFound)
+			},
+			ingResultFn: func() *engif.Result {
+				var jReply any
+				if err := json.NewDecoder(strings.NewReader(notFoundReply)).Decode(&jReply); err != nil {
+					return nil
+				}
+
+				return &engif.Result{
+					Object: jReply,
+				}
+			},
+			wantErr: false,
+		},
+		{
+			name: "test http error",
+			newIngArgs: newRestIngestArgs{
+				restCfg: &pb.RestType{
+					Endpoint: `/repos/{{.Entity.Owner}}/{{.Entity.Name}}/branches/{{ index .Params "branch" }}/protection`,
+					Parse:    "json",
+				},
+			},
+			ingArgs: ingestArgs{
+				ent: &pb.Repository{
+					Owner:  "OwnerVar",
+					Name:   "NameVar",
+					RepoId: 456,
+				},
+				params: map[string]any{
+					"branch": "main",
+				},
+			},
+			testHandler: func(writer http.ResponseWriter, request *http.Request) {
+				assert.Equal(t, "/repos/OwnerVar/NameVar/branches/main/protection", request.URL.Path, "unexpected path")
+				assert.Equal(t, http.MethodGet, request.Method, "unexpected method")
+
+				writer.WriteHeader(http.StatusForbidden)
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			testServer := httptest.NewServer(tt.testHandler)
+			defer testServer.Close()
+			tt.newIngArgs.pbuild = testGithubProviderBuilder(testServer.URL)
+
+			engine, err := NewRestRuleDataIngest(tt.newIngArgs.restCfg, tt.newIngArgs.pbuild)
+			require.NoError(t, err, "unexpected error creating ingestion engine")
+			require.NotNil(t, engine, "expected non-nil ingestion engine")
+
+			result, err := engine.Ingest(context.Background(), tt.ingArgs.ent, tt.ingArgs.params)
+			if tt.wantErr {
+				require.Error(t, err, "expected error")
+				return
+			}
+
+			require.NoError(t, err, "unexpected error creating remediate engine")
+			require.Equal(t, tt.ingResultFn(), result, "unexpected result")
 		})
 	}
 }
