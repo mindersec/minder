f254eba2db416be8d94aa35bcf3a1c41b6a6926c
Jakub Hrozek <jakub@stacklok.com>
2023-10-12 12:05:33 +0200

    Add a test for the gh branch protection remediator

---
+++ internal/engine/actions/remediate/gh_branch_protect/gh_branch_protect_test.go
@@ -0,0 +1,281 @@
+// Copyright 2023 Stacklok, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package gh_branch_protect provides the github branch protection remediation engine
+package gh_branch_protect
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-github/v53/github"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/reflect/protoreflect"
+
+	"github.com/stacklok/mediator/internal/db"
+	"github.com/stacklok/mediator/internal/engine/interfaces"
+	"github.com/stacklok/mediator/internal/providers"
+	mock_ghclient "github.com/stacklok/mediator/internal/providers/github/mock"
+	pb "github.com/stacklok/mediator/pkg/api/protobuf/go/mediator/v1"
+	provifv1 "github.com/stacklok/mediator/pkg/providers/v1"
+)
+
+const (
+	ghApiUrl         = "https://api.github.com"
+	reviewCountPatch = `{"required_pull_request_reviews":{"required_approving_review_count":{{ .Profile.required_approving_review_count }}}}`
+
+	repoOwner = "stacklok"
+	repoName  = "mediator"
+)
+
+var TestActionTypeValid interfaces.ActionType = "remediate-test"
+
+func testGithubProviderBuilder(baseURL string) *providers.ProviderBuilder {
+	if !strings.HasSuffix(baseURL, "/") {
+		baseURL = baseURL + "/"
+	}
+
+	definitionJSON := `{
+		"github": {
+			"endpoint": "` + baseURL + `"
+		}
+	}`
+
+	return providers.NewProviderBuilder(
+		&db.Provider{
+			Name:       "github",
+			Version:    provifv1.V1,
+			Implements: []db.ProviderType{db.ProviderTypeGithub, db.ProviderTypeRest},
+			Definition: json.RawMessage(definitionJSON),
+		},
+		db.ProviderAccessToken{},
+		"token",
+	)
+}
+
+type protectionRequestMatcher struct {
+	exp *github.ProtectionRequest
+}
+
+func (m *protectionRequestMatcher) Matches(x interface{}) bool {
+	req, ok := x.(*github.ProtectionRequest)
+	if !ok {
+		return false
+	}
+
+	if m.exp.AllowForcePushes != nil {
+		if req.AllowForcePushes == nil {
+			return false
+		}
+
+		if *req.AllowForcePushes != *m.exp.AllowForcePushes {
+			return false
+		}
+	}
+
+	return req.RequiredPullRequestReviews.RequiredApprovingReviewCount == m.exp.RequiredPullRequestReviews.RequiredApprovingReviewCount &&
+		req.AllowDeletions == m.exp.AllowDeletions
+}
+
+func (m *protectionRequestMatcher) String() string {
+	return fmt.Sprintf("is equivalent to proto %+v", m.exp)
+}
+
+func eqProtectionRequest(exp *github.ProtectionRequest) gomock.Matcher {
+	return &protectionRequestMatcher{exp}
+}
+
+func TestBranchProtectionRemediate(t *testing.T) {
+	t.Parallel()
+
+	ctrl := gomock.NewController(t)
+	t.Cleanup(func() {
+		ctrl.Finish()
+	})
+
+	type remediateArgs struct {
+		remAction interfaces.ActionOpt
+		ent       protoreflect.ProtoMessage
+		pol       map[string]any
+		params    map[string]any
+	}
+
+	type newBranchProtectionRemediateArgs struct {
+		ghp        *pb.RuleType_Definition_Remediate_GhBranchProtectionType
+		pbuild     *providers.ProviderBuilder
+		actionType interfaces.ActionType
+	}
+
+	tests := []struct {
+		name        string
+		newRemArgs  *newBranchProtectionRemediateArgs
+		remArgs     *remediateArgs
+		mockSetup   func(*mock_ghclient.MockGitHub)
+		wantErr     bool
+		wantInitErr bool
+	}{
+		{
+			name: "invalid action type",
+			newRemArgs: &newBranchProtectionRemediateArgs{
+				ghp: &pb.RuleType_Definition_Remediate_GhBranchProtectionType{
+					Patch: reviewCountPatch,
+				},
+				pbuild:     testGithubProviderBuilder(ghApiUrl),
+				actionType: "",
+			},
+			mockSetup: func(mockGitHub *mock_ghclient.MockGitHub) {
+			},
+			remArgs: &remediateArgs{
+				remAction: interfaces.ActionOptOn,
+				ent: &pb.Repository{
+					Owner: repoOwner,
+					Name:  repoName,
+				},
+				pol: map[string]any{
+					"required_approving_review_count": 2,
+				},
+				params: map[string]any{
+					"branch": "main",
+				},
+			},
+			wantInitErr: true,
+		},
+		{
+			name: "No protection was in place",
+			newRemArgs: &newBranchProtectionRemediateArgs{
+				ghp: &pb.RuleType_Definition_Remediate_GhBranchProtectionType{
+					Patch: reviewCountPatch,
+				},
+				pbuild:     testGithubProviderBuilder(ghApiUrl),
+				actionType: TestActionTypeValid,
+			},
+			remArgs: &remediateArgs{
+				remAction: interfaces.ActionOptOn,
+				ent: &pb.Repository{
+					Owner: repoOwner,
+					Name:  repoName,
+				},
+				pol: map[string]any{
+					"required_approving_review_count": 2,
+				},
+				params: map[string]any{
+					"branch": "main",
+				},
+			},
+			mockSetup: func(mockGitHub *mock_ghclient.MockGitHub) {
+				mockGitHub.EXPECT().
+					GetBranchProtection(gomock.Any(), repoOwner, repoName, "main").
+					Return(nil, github.ErrBranchNotProtected)
+				mockGitHub.EXPECT().
+					UpdateBranchProtection(gomock.Any(), repoOwner, repoName, "main",
+						// nested pointers to structs confuse gmock
+						eqProtectionRequest(
+							&github.ProtectionRequest{
+								RequiredPullRequestReviews: &github.PullRequestReviewsEnforcementRequest{
+									RequiredApprovingReviewCount: 2,
+								},
+							},
+						),
+					).
+					Return(nil)
+			},
+		},
+		{
+			name: "Some protection was in place, remediator merges the patch",
+			newRemArgs: &newBranchProtectionRemediateArgs{
+				ghp: &pb.RuleType_Definition_Remediate_GhBranchProtectionType{
+					Patch: reviewCountPatch,
+				},
+				pbuild:     testGithubProviderBuilder(ghApiUrl),
+				actionType: TestActionTypeValid,
+			},
+			remArgs: &remediateArgs{
+				remAction: interfaces.ActionOptOn,
+				ent: &pb.Repository{
+					Owner: repoOwner,
+					Name:  repoName,
+				},
+				pol: map[string]any{
+					"required_approving_review_count": 2,
+				},
+				params: map[string]any{
+					"branch": "main",
+				},
+			},
+			mockSetup: func(mockGitHub *mock_ghclient.MockGitHub) {
+				mockGitHub.EXPECT().
+					GetBranchProtection(gomock.Any(), repoOwner, repoName, "main").
+					Return(
+						&github.Protection{
+							RequiredPullRequestReviews: &github.PullRequestReviewsEnforcement{
+								RequiredApprovingReviewCount: 1,
+							},
+							AllowForcePushes: &github.AllowForcePushes{Enabled: true},
+						},
+						nil)
+
+				mockGitHub.EXPECT().
+					UpdateBranchProtection(gomock.Any(), repoOwner, repoName, "main",
+						// nested pointers to structs confuse gmock
+						eqProtectionRequest(
+							&github.ProtectionRequest{
+								RequiredPullRequestReviews: &github.PullRequestReviewsEnforcementRequest{
+									RequiredApprovingReviewCount: 2,
+								},
+								AllowForcePushes: github.Bool(true),
+							},
+						),
+					).
+					Return(nil)
+			},
+		},
+	}
+
+	mockClient := mock_ghclient.NewMockGitHub(ctrl)
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			engine, err := NewGhBranchProtectRemediator(tt.newRemArgs.actionType, tt.newRemArgs.ghp, tt.newRemArgs.pbuild)
+			if tt.wantInitErr {
+				require.Error(t, err, "expected error")
+				return
+			}
+
+			require.NoError(t, err, "unexpected error creating remediate engine")
+			// TODO(jakub): providerBuilder should be an interface so we can pass in mock more easily
+			engine.cli = mockClient
+
+			require.NoError(t, err, "unexpected error creating remediate engine")
+			require.NotNil(t, engine, "expected non-nil remediate engine")
+
+			tt.mockSetup(mockClient)
+
+			err = engine.Do(context.Background(), interfaces.ActionCmdOn, tt.remArgs.remAction, tt.remArgs.ent, tt.remArgs.pol, tt.remArgs.params)
+			if tt.wantErr {
+				require.Error(t, err, "expected error")
+				return
+			}
+
+			require.NoError(t, err, "unexpected error running remediate engine")
+		})
+	}
+}
