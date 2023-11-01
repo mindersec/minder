4de0296677c1fc81bf8b3cd12972439cca299d43
Radoslav Dimitrov <radoslav@stacklok.com>
2023-11-01 22:45:55 +0200

    Update license header for handlers_ruletype.go
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>

--- internal/controlplane/handlers_ruletype.go
+++ internal/controlplane/handlers_ruletype.go
@@ -1,3 +1,17 @@
+// Copyright 2023 Stacklok, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package controlplane
 
 import (
@@ -17,8 +31,6 @@ import (
 	"github.com/stacklok/mediator/internal/util"
 	minderv1 "github.com/stacklok/mediator/pkg/api/protobuf/go/minder/v1"
 )
-
-// Rule type CRUD
 
 // ListRuleTypes is a method to list all rule types for a given context
 func (s *Server) ListRuleTypes(
