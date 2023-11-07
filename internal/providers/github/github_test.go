9f0c10d4f4d866c3fd88de10a95022f5ca61b5de
Jakub Hrozek <jakub@stacklok.com>
2023-11-07 12:07:12 +0100

    add a unit test

--- internal/providers/github/github_test.go
+++ internal/providers/github/github_test.go
@@ -16,6 +16,8 @@ package github
 
 import (
 	"context"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -35,4 +37,82 @@ func TestNewRestClient(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.NotNil(t, client)
+}
+
+func TestArtifactAPIEscapes(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name        string
+		testHandler http.HandlerFunc
+		cliFn       func(cli *RestClient)
+		wantErr     bool
+	}{
+		{
+			name: "GetPackageByName escapes the package name",
+			testHandler: func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "/orgs/stacklok/packages/container/helm%2Fmediator", r.URL.RequestURI())
+				w.WriteHeader(http.StatusOK)
+			},
+			cliFn: func(cli *RestClient) {
+				_, err := cli.GetPackageByName(context.Background(), true, "stacklok", "container", "helm/mediator")
+				assert.NoError(t, err)
+			},
+		},
+		{
+			name: "GetPackageVersions escapes the package name",
+			testHandler: func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "/orgs/stacklok/packages/container/helm%2Fmediator/versions?package_type=container&state=active", r.URL.RequestURI())
+				w.WriteHeader(http.StatusOK)
+			},
+			cliFn: func(cli *RestClient) {
+				_, err := cli.GetPackageVersions(context.Background(), true, "stacklok", "container", "helm/mediator")
+				assert.NoError(t, err)
+			},
+		},
+		{
+			name: "GetPackageVersionByTag escapes the package name",
+			testHandler: func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "/orgs/stacklok/packages/container/helm%2Fmediator/versions?package_type=container&state=active", r.URL.RequestURI())
+				w.WriteHeader(http.StatusOK)
+			},
+			cliFn: func(cli *RestClient) {
+				_, err := cli.GetPackageVersionByTag(context.Background(), true, "stacklok", "container", "helm/mediator", "v1.0.0")
+				assert.NoError(t, err)
+			},
+		},
+		{
+			name: "GetPackageVersionByID escapes the package name",
+			testHandler: func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "/orgs/stacklok/packages/container/helm%2Fmediator/versions/123", r.URL.RequestURI())
+				w.WriteHeader(http.StatusOK)
+			},
+			cliFn: func(cli *RestClient) {
+				_, err := cli.GetPackageVersionById(context.Background(), true, "stacklok", "container", "helm/mediator", 123)
+				assert.NoError(t, err)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			testServer := httptest.NewServer(tt.testHandler)
+			defer testServer.Close()
+
+			client, err := NewRestClient(context.Background(), &minderv1.GitHubProviderConfig{
+				Endpoint: testServer.URL + "/",
+			},
+				provtelemetry.NewNoopMetrics(),
+				"token", "")
+			assert.NoError(t, err)
+			assert.NotNil(t, client)
+
+			tt.cliFn(client)
+		})
+	}
+
 }
