6c04f7a50c15c8cb429d37d6aa47b7586e76afda
Juan Antonio Osorio <ozz@stacklok.com>
2023-12-18 10:29:51 +0200

    Remove invalid validation for projects in reconcilers (#1947)
    These were still validating integers while they're not UUIDs. This also
    moved one event reference to be `project` instead of `group`.

--- internal/reconcilers/artifacts.go
+++ internal/reconcilers/artifacts.go
@@ -47,7 +47,7 @@ var (
 // RepoReconcilerEvent is an event that is sent to the reconciler topic
 type RepoReconcilerEvent struct {
 	// Project is the project that the event is relevant to
-	Project uuid.UUID `json:"group" validate:"gte=0"`
+	Project uuid.UUID `json:"project"`
 	// Repository is the repository to be reconciled
 	Repository int32 `json:"repository" validate:"gte=0"`
 }
