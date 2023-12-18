888f7cf18dd13dd2493bc48745b6526871aba898
Jakub Hrozek <jakub@stacklok.com>
2023-12-18 11:05:34 +0100

    Don't attempt to reopen a PR after updating a branch if it already exists (#1944)
    The PR remediation was checking if a PR with the same contents already
    exist as a fast and cheap check to avoid triggering the remediation
    again. But in case the profile changes or the contents change for
    another reason, we want to push to the existing branch to update it, but
    don't try to open a PR again as that would fail with 422.
    
    To do that, this patch adds a check if a PR is already open from the
    branch we just pushed to and if it is, just returns before opening the
    PR again.

--- internal/engine/actions/remediate/pull_request/pull_request.go
+++ internal/engine/actions/remediate/pull_request/pull_request.go
@@ -207,7 +207,7 @@ func (r *Remediator) Do(
 	var remErr error
 	switch remAction {
 	case interfaces.ActionOptOn:
-		alreadyExists, err := prAlreadyExists(ctx, r.ghCli, repo, magicComment)
+		alreadyExists, err := prWithContentAlreadyExists(ctx, r.ghCli, repo, magicComment)
 		if err != nil {
 			return nil, fmt.Errorf("cannot check if PR already exists: %w", err)
 		}
@@ -318,6 +318,19 @@ func (r *Remediator) runGit(
 		return fmt.Errorf("cannot push: %w", err)
 	}
 	zerolog.Ctx(ctx).Debug().Msgf("Push output: %s", b.String())
+
+	// if a PR from this branch already exists, don't create a new one
+	// this handles the case where the content changed (e.g. profile changed)
+	// but the PR was not closed
+	prAlreadyExists, err := prFromBranchAlreadyExists(ctx, r.ghCli, pbRepo, branchBaseName(title))
+	if err != nil {
+		return fmt.Errorf("cannot check if PR from branch already exists: %w", err)
+	}
+
+	if prAlreadyExists {
+		zerolog.Ctx(ctx).Info().Msg("PR from branch already exists, won't create a new one")
+		return nil
+	}
 
 	_, err = r.ghCli.CreatePullRequest(
 		ctx, pbRepo.GetOwner(), pbRepo.GetName(),
@@ -434,13 +447,12 @@ func createReviewBody(prText, magicComment string) (string, error) {
 }
 
 // returns true if an open PR with the magic comment already exists
-func prAlreadyExists(
+func prWithContentAlreadyExists(
 	ctx context.Context,
 	cli provifv1.GitHub,
 	repo *pb.Repository,
 	magicComment string,
 ) (bool, error) {
-	// TODO(jakub): pagination
 	openPrs, err := cli.ListPullRequests(ctx, repo.GetOwner(), repo.GetName(), &github.PullRequestListOptions{})
 	if err != nil {
 		return false, fmt.Errorf("cannot list pull requests: %w", err)
@@ -452,4 +464,23 @@ func prAlreadyExists(
 		}
 	}
 	return false, nil
+}
+
+func prFromBranchAlreadyExists(
+	ctx context.Context,
+	cli provifv1.GitHub,
+	repo *pb.Repository,
+	branchName string,
+) (bool, error) {
+	// TODO(jakub): pagination
+	opts := &github.PullRequestListOptions{
+		Head: branchName,
+	}
+
+	openPrs, err := cli.ListPullRequests(ctx, repo.GetOwner(), repo.GetName(), opts)
+	if err != nil {
+		return false, fmt.Errorf("cannot list pull requests: %w", err)
+	}
+
+	return len(openPrs) > 0, nil
 }
