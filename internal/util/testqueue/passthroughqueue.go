582fa2fac1d31367b86754bcaf96c1fa7cd1af59
Juan Antonio Osorio <ozz@stacklok.com>
2023-12-22 13:11:45 +0200

    entity event handler test fixes (#2026)
    * Wait for eventer to start  before running an execution
    
    The idea is that the eventer should be ready to issue events before we
    try to parse and process an event.
    
    This also adds another log line to know if the test is blocked because of the
    last `Wait` call.
    
    Closes https://github.com/stacklok/minder/issues/1710
    
    * Add logs for the mock message queue receiver for testing
    
    * Ensure topic is registered before eventer runs

--- internal/util/testqueue/passthroughqueue.go
+++ internal/util/testqueue/passthroughqueue.go
@@ -16,18 +16,26 @@
 // Package testqueue contains queue utilities for testing
 package testqueue
 
-import "github.com/ThreeDotsLabs/watermill/message"
+import (
+	"testing"
+
+	"github.com/ThreeDotsLabs/watermill/message"
+)
 
 // PassthroughQueue is a queue that passes messages through.
 // It's only useful for testing.
 type PassthroughQueue struct {
 	ch chan *message.Message
+	t  *testing.T
 }
 
 // NewPassthroughQueue creates a new PassthroughQueue
-func NewPassthroughQueue() *PassthroughQueue {
+func NewPassthroughQueue(t *testing.T) *PassthroughQueue {
+	t.Helper()
+
 	return &PassthroughQueue{
 		ch: make(chan *message.Message),
+		t:  t,
 	}
 }
 
@@ -38,6 +46,7 @@ func (q *PassthroughQueue) GetQueue() <-chan *message.Message {
 
 // Pass passes a message through the queue
 func (q *PassthroughQueue) Pass(msg *message.Message) error {
+	q.t.Logf("Passing message through queue: %s", msg.UUID)
 	q.ch <- msg
 	return nil
 }
