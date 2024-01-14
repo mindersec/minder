629421615886cf4d614b5a0fd306c662688000d3
Vyom-Yadav <jackhammervyom@gmail.com>
2024-01-14 23:47:45 +0530

    minor: Remove incorrect package comment
    Signed-off-by: Vyom-Yadav <jackhammervyom@gmail.com>

--- internal/engine/ingestcache/interface.go
+++ internal/engine/ingestcache/interface.go
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// Package rule provides the CLI subcommand for managing rules
 
 // Package ingestcache a cache that is used to cache the results of ingesting data.
 // The intent is to reduce the number of calls to external services.
