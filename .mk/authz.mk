662c627743ec9b17e6bc1aae369470ee10800db0
Juan Antonio Osorio <ozz@stacklok.com>
2024-01-22 13:56:24 +0200

    Add authorization JSON model workflow
    The OpenFGA API does not know how to handle the DSL natively and relies
    on users tranforming the DSL into JSON. So, this adds a Makefile target
    to do so. We also add the generated JSON structure into our repo.
    
    The generation now runs as part of the general `make gen` target.
    
    This also add a validation step that runs before the authz tests.

--- .mk/authz.mk
+++ .mk/authz.mk
@@ -1,10 +1,25 @@
 FGA_TESTS_DIR := internal/authz/model/tests
 FGA_TEST_FILES := $(wildcard $(FGA_TESTS_DIR)/*.tests.yaml)
 
+FGA_MODEL := internal/authz/model/minder.fga
+FGA_JSON_MODEL := internal/authz/model/minder.generated.json
+
 .PHONY: authz-tests
-authz-tests: ## run authz tests
+authz-tests: authz-validate ## run authz tests
 	@for file in $(FGA_TEST_FILES); do \
 		echo "* Running tests in $$file"; \
 		fga model test --tests $$file; \
 		echo ""; \
 	done
+
+.PHONY: authz-validate
+authz-validate:  ## validate the authz model
+	@echo "* Validating authz model"
+	@fga model validate --file internal/authz/model/minder.fga
+
+.PHONY: authz-model
+authz-model: authz-validate $(FGA_JSON_MODEL) ## validate and generate the authz model
+
+$(FGA_JSON_MODEL): $(FGA_MODEL)
+	@echo "* Generating JSON model"
+	@fga model transform --file $(FGA_MODEL) | tee $(FGA_JSON_MODEL)
