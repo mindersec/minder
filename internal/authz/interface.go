e052e1cd4179fc7440dac1824694b8b3a2b71221
Juan Antonio Osorio <ozz@stacklok.com>
2024-01-23 10:54:32 +0200

    Skeleton of Authz client interface to enable integration (#2175)
    * Skeleton of Authz client interface to enable integration
    
    * Rename fake to mock per feedback
    
    * Address linting issues
    
    ---------
    
    Co-authored-by: Evan Anderson <evan@stacklok.com>

---
+++ internal/authz/interface.go
@@ -0,0 +1,59 @@
+//
+// Copyright 2023 Stacklok, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package authz provides the authorization utilities for minder
+package authz
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+// ErrNotAuthorized is the error returned when a user is not authorized to perform an action
+var ErrNotAuthorized = fmt.Errorf("not authorized")
+
+// Role is the role a user can have on a project
+type Role string
+
+const (
+	// AuthzRoleAdmin is the admin role
+	AuthzRoleAdmin Role = "admin"
+	// AuthzRoleEditor is the editor role
+	AuthzRoleEditor Role = "editor"
+	// AuthzRoleViewer is the viewer role
+	AuthzRoleViewer Role = "viewer"
+)
+
+// Client provides an abstract interface which simplifies interacting with
+// OpenFGA and supports no-op and fake implementations.
+type Client interface {
+	// Check returns a NotAuthorized if the action is not allowed on the resource, or nil if it is allowed
+	Check(ctx context.Context, action string, project uuid.UUID) error
+
+	// Write stores an authorization tuple allowing user (an OAuth2 subject) to
+	// act in the specified role on the project.
+	//
+	// NOTE: this method _DOES NOT CHECK_ that the current user in the context
+	// has permissions to update the project.
+	Write(ctx context.Context, user string, role Role, project uuid.UUID) error
+	// Delete removes an authorization from user (an OAuth2 subject) to act in
+	// the specified role on the project.
+	//
+	// NOTE: this method _DOES NOT CHECK_ that the current user in the context
+	// has permissions to update the project.
+	Delete(ctx context.Context, user string, role Role, project uuid.UUID) error
+}
