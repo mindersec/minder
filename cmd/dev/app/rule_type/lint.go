7762c927ed8427a20eadb36f774a23af38f55ed0
Juan Antonio Osorio <ozz@stacklok.com>
2024-03-05 17:06:54 +0200

    Allow rule type linting to skip rego and read many rule types (#2499)
    Signed-off-by: Juan Antonio Osorio <ozz@stacklok.com>

--- cmd/dev/app/rule_type/lint.go
+++ cmd/dev/app/rule_type/lint.go
@@ -41,6 +41,7 @@ func CmdLint() *cobra.Command {
 		SilenceUsage: true,
 	}
 	lintCmd.Flags().StringP("rule-type", "r", "", "file to read rule type definition from")
+	lintCmd.Flags().BoolP("skip-rego", "s", false, "skip rego rule validation")
 
 	if err := lintCmd.MarkFlagRequired("rule-type"); err != nil {
 		fmt.Fprintf(os.Stderr, "Error marking flag as required: %s\n", err)
@@ -52,30 +53,63 @@ func CmdLint() *cobra.Command {
 
 func lintCmdRun(cmd *cobra.Command, _ []string) error {
 	rtpath := cmd.Flag("rule-type")
+	skipRego := cmd.Flag("skip-rego").Value.String() == "true"
 
 	ctx := cmd.Context()
 
 	rtpathStr := rtpath.Value.String()
 
-	rt, err := readRuleTypeFromFile(rtpathStr)
-	if err != nil {
-		return fmt.Errorf("error reading rule type from file: %w", err)
-	}
-
-	if err := rt.Validate(); err != nil {
-		return fmt.Errorf("error validating rule type: %w", err)
-	}
-
-	// get file name without extension
-	ruleName := strings.TrimSuffix(filepath.Base(rtpathStr), filepath.Ext(rtpathStr))
-	if rt.Name != ruleName {
-		return fmt.Errorf("rule type name does not match file name: %s != %s", rt.Name, ruleName)
-	}
-
-	if rt.Def.Eval.Type == rego.RegoEvalType {
-		if err := validateRegoRule(ctx, rt.Def.Eval.Rego, rtpathStr, cmd.OutOrStdout()); err != nil {
-			return fmt.Errorf("failed validating rego rule: %w", err)
+	var errors []error
+	walkerr := filepath.Walk(rtpathStr, func(path string, info os.FileInfo, walkerr error) error {
+		if walkerr != nil {
+			return fmt.Errorf("error walking path %s: %w", path, walkerr)
 		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if filepath.Ext(path) != ".yaml" && filepath.Ext(path) != ".yml" {
+			return nil
+		}
+
+		rt, err := readRuleTypeFromFile(path)
+		if err != nil {
+			errors = append(errors, fmt.Errorf("error reading rule type from file %s: %w", path, err))
+			return nil
+		}
+
+		if err := rt.Validate(); err != nil {
+			errors = append(errors, fmt.Errorf("error validating rule type: %w", err))
+			return nil
+		}
+
+		// get file name without extension
+		ruleName := strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
+		if rt.Name != ruleName {
+			errors = append(errors, fmt.Errorf("rule type name does not match file name: %s != %s", rt.Name, ruleName))
+			return nil
+		}
+
+		if rt.Def.Eval.Type == rego.RegoEvalType && !skipRego {
+			if err := validateRegoRule(ctx, rt.Def.Eval.Rego, rtpathStr, cmd.OutOrStdout()); err != nil {
+				errors = append(errors, fmt.Errorf("failed validating rego rule: %w", err))
+				return nil
+			}
+		}
+
+		return nil
+	})
+
+	if walkerr != nil {
+		return fmt.Errorf("error walking path %s: %w", rtpathStr, walkerr)
+	}
+
+	if len(errors) > 0 {
+		for _, err := range errors {
+			fmt.Fprintln(cmd.ErrOrStderr(), err)
+		}
+		return fmt.Errorf("failed linting rule type")
 	}
 
 	return nil
