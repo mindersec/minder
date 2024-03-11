10ce82947ce21d3f490eed09635f0fa6e25cae57
Juan Antonio Osorio <ozz@stacklok.com>
2024-03-11 10:25:34 +0200

    Add down migration and ensure data integrity
    Signed-off-by: Juan Antonio Osorio <ozz@stacklok.com>

--- database/migrations/000026_hierarchy.up.sql
+++ database/migrations/000026_hierarchy.up.sql
@@ -12,8 +12,15 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-ALTER TABLE repositories
-  ADD COLUMN provider_id UUID REFERENCES providers(id) ON DELETE CASCADE;
+--- Add unique constraint to the providers table between name and id
+ALTER TABLE providers
+  ADD CONSTRAINT providers_name_id_key
+  UNIQUE (name, id);
+
+-- Initialize the provider_id column in the repositories table.
+-- Note that the foreign key constraint is not added here, as it will be added
+-- after the provider_id column is populated.
+ALTER TABLE repositories ADD COLUMN provider_id UUID;
 
 -- set the provider_id for all existing repositories
 UPDATE repositories
@@ -24,6 +31,14 @@ UPDATE repositories
 -- Make provider_id not nullable
 ALTER TABLE repositories
   ALTER COLUMN provider_id SET NOT NULL;
+
+-- Now let's add the foreign key constraint in the repositories table
+-- for the provider_id and provider columns
+ALTER TABLE repositories
+  ADD CONSTRAINT repositories_provider_id_name_fkey
+  FOREIGN KEY (provider_id, provider)
+  REFERENCES providers(id, name)
+  ON DELETE CASCADE;
 
 -- change the project_id column from being a foreign key from the providers
 -- table to being a foreign key from the projects table
@@ -40,7 +55,7 @@ ALTER TABLE repositories
 -- Now let's do the same for the rule_type table
 
 ALTER TABLE rule_type
-  ADD COLUMN provider_id UUID REFERENCES providers(id) ON DELETE CASCADE;
+  ADD COLUMN provider_id UUID;
 
 -- set the provider_id for all existing rule_type
 UPDATE rule_type
@@ -51,6 +66,14 @@ UPDATE rule_type
 -- Make provider_id not nullable
 ALTER TABLE rule_type
   ALTER COLUMN provider_id SET NOT NULL;
+
+-- Now let's add the foreign key constraint in the rule_type table
+-- for the provider_id and provider columns
+ALTER TABLE rule_type
+  ADD CONSTRAINT rule_type_provider_id_name_fkey
+  FOREIGN KEY (provider_id, provider)
+  REFERENCES providers(id, name)
+  ON DELETE CASCADE;
 
 -- change the project_id column from being a foreign key from the providers
 -- table to being a foreign key from the projects table
@@ -67,7 +90,7 @@ ALTER TABLE rule_type
 -- Now let's cover the `profiles` table
 
 ALTER TABLE profiles
-  ADD COLUMN provider_id UUID REFERENCES providers(id) ON DELETE CASCADE;
+  ADD COLUMN provider_id UUID;
 
 -- set the provider_id for all existing profiles
 UPDATE profiles
@@ -78,6 +101,14 @@ UPDATE profiles
 -- Make provider_id not nullable
 ALTER TABLE profiles
   ALTER COLUMN provider_id SET NOT NULL;
+
+-- Now let's add the foreign key constraint in the profiles table
+-- for the provider_id and provider columns
+ALTER TABLE profiles
+  ADD CONSTRAINT profiles_provider_id_name_fkey
+  FOREIGN KEY (provider_id, provider)
+  REFERENCES providers(id, name)
+  ON DELETE CASCADE;
 
 -- change the project_id column from being a foreign key from the providers
 -- table to being a foreign key from the projects table
