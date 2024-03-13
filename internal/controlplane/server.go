6ac8893ec4a3f2ae140e99460fecc65a7fcb35e5
Don Browne <dmjb@users.noreply.github.com>
2024-03-13 15:51:54 +0000

    Move repo deletion into a separate interface (#2608)
    This is the first of a two part process of moving repo management logic
    into a separate interface. This PR moves the logic for the two repo
    deletion endpoints behind an interface, and provides tests for both the
    deletion logic as well as the endpoints which call it.

--- internal/controlplane/server.go
+++ internal/controlplane/server.go
@@ -57,6 +57,7 @@ import (
 	"github.com/stacklok/minder/internal/profiles"
 	"github.com/stacklok/minder/internal/providers/ratecache"
 	provtelemetry "github.com/stacklok/minder/internal/providers/telemetry"
+	"github.com/stacklok/minder/internal/repositories/github"
 	"github.com/stacklok/minder/internal/repositories/github/webhooks"
 	"github.com/stacklok/minder/internal/ruletypes"
 	"github.com/stacklok/minder/internal/util"
@@ -89,8 +90,11 @@ type Server struct {
 	// inject more entity-specific interfaces. For example, we may want to
 	// consider having a struct per grpc service
 	profileValidator *profiles.Validator
-	webhookManager   webhooks.WebhookManager
 	ruleTypes        ruletypes.RuleTypeService
+	repos            github.RepositoryService
+	// TODO: this will be removed from server when the create repo
+	// flow is refactored
+	webhookManager webhooks.WebhookManager
 
 	// Implementations for service registration
 	pb.UnimplementedHealthServiceServer
@@ -148,6 +152,7 @@ func NewServer(
 	if err != nil {
 		return nil, fmt.Errorf("failed to create crypto engine: %w", err)
 	}
+	whManager := webhooks.NewWebhookManager(cfg.WebhookConfig)
 	s := &Server{
 		store:               store,
 		cfg:                 cfg,
@@ -158,8 +163,9 @@ func NewServer(
 		mt:                  metrics.NewNoopMetrics(),
 		provMt:              provtelemetry.NewNoopMetrics(),
 		profileValidator:    profiles.NewValidator(store),
-		webhookManager:      webhooks.NewWebhookManager(cfg.WebhookConfig),
 		ruleTypes:           ruletypes.NewRuleTypeService(store),
+		repos:               github.NewRepositoryService(whManager, store, evt),
+		webhookManager:      whManager,
 		// TODO: this currently always returns authorized as a transitionary measure.
 		// When OpenFGA is fully rolled out, we may want to make this a hard error or set to false.
 		authzClient: &mock.NoopClient{Authorized: true},
