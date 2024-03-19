d93d4259dcf802b44a391d85166f14dec7e7cf66
Jakub Hrozek <jakub@stacklok.com>
2024-03-19 16:08:07 +0100

    Add the ruletype severity when returning the list of evaluations
    Instead of empty severity, let's return the one from the ruletype
    
    Fixes: #2709

--- internal/controlplane/handlers_evalstatus.go
+++ internal/controlplane/handlers_evalstatus.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/rs/zerolog"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -123,7 +124,7 @@ func sortEntitiesEvaluationStatus(
 				profileStatuses[p.ID] = buildProfileStatus(&p, profileStatusList)
 			}
 
-			stat := buildRuleEvaluationStatusFromDBEvaluation(&p, e)
+			stat := buildRuleEvaluationStatusFromDBEvaluation(ctx, &p, e)
 			if _, ok := statusByEntity[entString]; !ok {
 				statusByEntity[entString] = make(map[uuid.UUID][]*minderv1.RuleEvaluationStatus)
 			}
@@ -258,6 +259,7 @@ func filterProfileLists(
 // buildRuleEvaluationStatusFromDBEvaluation converts from an evaluation and a
 // profile from the database to a minder RuleEvaluationStatus
 func buildRuleEvaluationStatusFromDBEvaluation(
+	ctx context.Context,
 	profile *db.ListProfilesByProjectIDRow, eval db.ListRuleEvaluationsByProfileIdRow,
 ) *minderv1.RuleEvaluationStatus {
 	guidance := ""
@@ -265,6 +267,15 @@ func buildRuleEvaluationStatusFromDBEvaluation(
 	if eval.EvalStatus.EvalStatusTypes == db.EvalStatusTypesFailure ||
 		eval.EvalStatus.EvalStatusTypes == db.EvalStatusTypesError {
 		guidance = eval.RuleTypeGuidance
+	}
+
+	sev := &minderv1.Severity{}
+	sev.EnsureDefault()
+	if err := sev.Value.FromString(string(eval.RuleTypeSeverityValue)); err != nil {
+		zerolog.Ctx(ctx).
+			Err(err).
+			Str("value", string(eval.RuleTypeSeverityValue)).
+			Msg("error converting severity will use defaults")
 	}
 
 	return &minderv1.RuleEvaluationStatus{
@@ -282,7 +293,7 @@ func buildRuleEvaluationStatusFromDBEvaluation(
 		RemediationDetails:     eval.RemDetails.String,
 		RuleTypeName:           eval.RuleTypeName,
 		Alert:                  buildEvalResultAlertFrom(&eval),
-		Severity:               &minderv1.Severity{},
+		Severity:               sev,
 	}
 }
 
