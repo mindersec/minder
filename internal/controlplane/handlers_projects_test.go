a519d3a12b099c0445c3fa5a8aa67ba3ae59845b
Juan Antonio Osorio <ozz@stacklok.com>
2024-03-22 08:21:04 +0200

    Detect project deletion while `ListProjects` gathers projects (#2752)
    Signed-off-by: Juan Antonio Osorio <ozz@stacklok.com>

--- internal/controlplane/handlers_projects_test.go
+++ internal/controlplane/handlers_projects_test.go
@@ -16,6 +16,7 @@ package controlplane
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 
 	"github.com/google/uuid"
@@ -61,4 +62,42 @@ func TestListProjects(t *testing.T) {
 
 	assert.Len(t, resp.Projects, 1)
 	assert.Equal(t, authzClient.Allowed[0].String(), resp.Projects[0].ProjectId)
+}
+
+func TestListProjectsWithOneDeletedWhileIterating(t *testing.T) {
+	t.Parallel()
+
+	user := openid.New()
+	assert.NoError(t, user.Set("sub", "testuser"))
+
+	authzClient := &mock.SimpleClient{
+		Allowed: []uuid.UUID{uuid.New(), uuid.New(), uuid.New()},
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockStore := mockdb.NewMockStore(ctrl)
+	mockStore.EXPECT().GetUserBySubject(gomock.Any(), user.Subject()).Return(db.User{ID: 1}, nil)
+	mockStore.EXPECT().GetProjectByID(gomock.Any(), authzClient.Allowed[0]).Return(
+		db.Project{ID: authzClient.Allowed[0]}, nil)
+	mockStore.EXPECT().GetProjectByID(gomock.Any(), authzClient.Allowed[1]).Return(
+		db.Project{}, sql.ErrNoRows)
+	mockStore.EXPECT().GetProjectByID(gomock.Any(), authzClient.Allowed[2]).Return(
+		db.Project{ID: authzClient.Allowed[2]}, nil)
+
+	server := Server{
+		store:       mockStore,
+		authzClient: authzClient,
+	}
+
+	ctx := context.Background()
+	ctx = auth.WithAuthTokenContext(ctx, user)
+
+	resp, err := server.ListProjects(ctx, &minder.ListProjectsRequest{})
+	assert.NoError(t, err)
+
+	assert.Len(t, resp.Projects, 2)
+	assert.Equal(t, authzClient.Allowed[0].String(), resp.Projects[0].ProjectId)
+	assert.Equal(t, authzClient.Allowed[2].String(), resp.Projects[1].ProjectId)
 }
