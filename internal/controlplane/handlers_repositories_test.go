20222582e0ce01b0306821b0ff23b1aee7cc22c9
Juan Antonio Osorio <ozz@stacklok.com>
2024-03-26 09:36:37 +0200

    Remove `StubGitHub` in favor of our existing mock
    This removes the manually-maintained `StubGitHub` in favor us using
    the auto-generated one.
    
    Signed-off-by: Juan Antonio Osorio <ozz@stacklok.com>

--- internal/controlplane/handlers_repositories_test.go
+++ internal/controlplane/handlers_repositories_test.go
@@ -18,11 +18,8 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"net/http"
 	"testing"
 
-	"github.com/go-git/go-git/v5"
-	"github.com/google/go-github/v56/github"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
@@ -33,6 +30,7 @@ import (
 	mockcrypto "github.com/stacklok/minder/internal/crypto/mock"
 	"github.com/stacklok/minder/internal/db"
 	"github.com/stacklok/minder/internal/engine"
+	mockgh "github.com/stacklok/minder/internal/providers/github/mock"
 	ghprovider "github.com/stacklok/minder/internal/providers/github/oauth"
 	"github.com/stacklok/minder/internal/providers/ratecache"
 	ghrepo "github.com/stacklok/minder/internal/repositories/github"
@@ -337,7 +335,10 @@ func createServer(
 	cancelable, cancel := context.WithCancel(context.Background())
 	clientCache := ratecache.NewRestClientCache(cancelable)
 	defer cancel()
-	clientCache.Set("", accessToken, db.ProviderTypeGithub, &StubGitHub{})
+
+	gh := mockgh.NewMockGitHub(ctrl)
+
+	clientCache.Set("", accessToken, db.ProviderTypeGithub, gh)
 
 	store := mockdb.NewMockStore(ctrl)
 	store.EXPECT().
@@ -367,209 +368,4 @@ func createServer(
 		restClientCache: clientCache,
 		cfg:             &server.Config{},
 	}
-}
-
-// TODO: get rid of the need to stub out the entire GitHub client interface
-type StubGitHub struct {
-	ExpectedOwner string
-	ExpectedRepo  string
-	T             *testing.T
-	Repo          *github.Repository
-	RepoErr       error
-	UserId        int64
-	ExistingHooks []*github.Hook
-	DeletedHooks  []int64
-	NewHooks      []*github.Hook
-}
-
-// FakeGitHub implements v1.GitHub.  Holy wide interface, Batman!
-var _ provinfv1.GitHub = (*StubGitHub)(nil)
-
-// CloseSecurityAdvisory implements v1.GitHub.
-func (*StubGitHub) CloseSecurityAdvisory(context.Context, string, string, string) error {
-	panic("unimplemented")
-}
-
-// CreateHook implements v1.GitHub.
-func (_ *StubGitHub) CreateHook(_ context.Context, _ string, _ string, _ *github.Hook) (*github.Hook, error) {
-	panic("unimplemented")
-}
-
-func (*StubGitHub) EditHook(context.Context, string, string, int64, *github.Hook) (*github.Hook, error) {
-	panic("unimplemented")
-}
-
-// CreatePullRequest implements v1.GitHub.
-func (*StubGitHub) CreatePullRequest(context.Context, string, string, string, string, string, string) (*github.PullRequest, error) {
-	panic("unimplemented")
-}
-
-// CreateReview implements v1.GitHub.
-func (*StubGitHub) CreateReview(context.Context, string, string, int, *github.PullRequestReviewRequest) (*github.PullRequestReview, error) {
-	panic("unimplemented")
-}
-
-// CreateSecurityAdvisory implements v1.GitHub.
-func (*StubGitHub) CreateSecurityAdvisory(context.Context, string, string, string, string, string, []*github.AdvisoryVulnerability) (string, error) {
-	panic("unimplemented")
-}
-
-// DeleteHook implements v1.GitHub.
-func (_ *StubGitHub) DeleteHook(_ context.Context, _ string, _ string, _ int64) (*github.Response, error) {
-	panic("unimplemented")
-}
-
-// DismissReview implements v1.GitHub.
-func (*StubGitHub) DismissReview(context.Context, string, string, int, int64, *github.PullRequestReviewDismissalRequest) (*github.PullRequestReview, error) {
-	panic("unimplemented")
-}
-
-// Do implements v1.GitHub.
-func (*StubGitHub) Do(context.Context, *http.Request) (*http.Response, error) {
-	panic("unimplemented")
-}
-
-// GetBaseURL implements v1.GitHub.
-func (*StubGitHub) GetBaseURL() string {
-	panic("unimplemented")
-}
-
-// GetBranchProtection implements v1.GitHub.
-func (*StubGitHub) GetBranchProtection(context.Context, string, string, string) (*github.Protection, error) {
-	panic("unimplemented")
-}
-
-// GetOwner implements v1.GitHub.
-func (*StubGitHub) GetOwner() string {
-	panic("unimplemented")
-}
-
-// GetPackageByName implements v1.GitHub.
-func (*StubGitHub) GetPackageByName(context.Context, bool, string, string, string) (*github.Package, error) {
-	panic("unimplemented")
-}
-
-// GetPackageVersionById implements v1.GitHub.
-func (*StubGitHub) GetPackageVersionById(context.Context, bool, string, string, string, int64) (*github.PackageVersion, error) {
-	panic("unimplemented")
-}
-
-// GetPackageVersionByTag implements v1.GitHub.
-func (*StubGitHub) GetPackageVersionByTag(context.Context, bool, string, string, string, string) (*github.PackageVersion, error) {
-	panic("unimplemented")
-}
-
-// GetPackageVersions implements v1.GitHub.
-func (*StubGitHub) GetPackageVersions(context.Context, bool, string, string, string) ([]*github.PackageVersion, error) {
-	panic("unimplemented")
-}
-
-// GetPullRequest implements v1.GitHub.
-func (*StubGitHub) GetPullRequest(context.Context, string, string, int) (*github.PullRequest, error) {
-	panic("unimplemented")
-}
-
-func (*StubGitHub) CreateIssueComment(context.Context, string, string, int, string) (*github.IssueComment, error) {
-	panic("unimplemented")
-}
-
-func (*StubGitHub) ListIssueComments(context.Context, string, string, int, *github.IssueListCommentsOptions) ([]*github.IssueComment, error) {
-	panic("unimplemented")
-}
-
-func (*StubGitHub) UpdateIssueComment(context.Context, string, string, int64, string) error {
-	panic("unimplemented")
-}
-
-func (*StubGitHub) UpdateReview(context.Context, string, string, int, int64, string) (*github.PullRequestReview, error) {
-	panic("unimplemented")
-}
-
-// GetRepository implements v1.GitHub.
-func (*StubGitHub) GetRepository(_ context.Context, _ string, _ string) (*github.Repository, error) {
-	panic("unimplemented")
-}
-
-// GetCredential implements v1.GitHub.
-func (*StubGitHub) GetCredential() provinfv1.GitHubCredential {
-	panic("unimplemented")
-}
-
-// ListAllRepositories implements v1.GitHub.
-func (*StubGitHub) ListAllRepositories(context.Context) ([]*pb.Repository, error) {
-	panic("unimplemented")
-}
-
-// ListFiles implements v1.GitHub.
-func (*StubGitHub) ListFiles(context.Context, string, string, int, int, int) ([]*github.CommitFile, *github.Response, error) {
-	panic("unimplemented")
-}
-
-// ListHooks implements v1.GitHub.
-func (_ *StubGitHub) ListHooks(_ context.Context, _ string, _ string) ([]*github.Hook, error) {
-	panic("unimplemented")
-}
-
-// ListOrganizationRepsitories implements v1.GitHub.
-func (*StubGitHub) ListOrganizationRepsitories(context.Context) ([]*pb.Repository, error) {
-	panic("unimplemented")
-}
-
-// ListPackagesByRepository implements v1.GitHub.
-func (*StubGitHub) ListPackagesByRepository(context.Context, bool, string, string, int64, int, int) ([]*github.Package, error) {
-	panic("unimplemented")
-}
-
-// ListPullRequests implements v1.GitHub.
-func (*StubGitHub) ListPullRequests(context.Context, string, string, *github.PullRequestListOptions) ([]*github.PullRequest, error) {
-	panic("unimplemented")
-}
-
-// ListReviews implements v1.GitHub.
-func (*StubGitHub) ListReviews(context.Context, string, string, int, *github.ListOptions) ([]*github.PullRequestReview, error) {
-	panic("unimplemented")
-}
-
-// NewRequest implements v1.GitHub.
-func (*StubGitHub) NewRequest(string, string, any) (*http.Request, error) {
-	panic("unimplemented")
-}
-
-// SetCommitStatus implements v1.GitHub.
-func (*StubGitHub) SetCommitStatus(context.Context, string, string, string, *github.RepoStatus) (*github.RepoStatus, error) {
-	panic("unimplemented")
-}
-
-// UpdateBranchProtection implements v1.GitHub.
-func (*StubGitHub) UpdateBranchProtection(context.Context, string, string, string, *github.ProtectionRequest) error {
-	panic("unimplemented")
-}
-
-// GetUserId implements v1.GitHub.
-func (s *StubGitHub) GetUserId(context.Context) (int64, error) {
-	return s.UserId, nil
-}
-
-// GetName implements v1.GitHub.
-func (*StubGitHub) GetName(context.Context) (string, error) {
-	panic("unimplemented")
-}
-
-// GetLogin implements v1.GitHub.
-func (*StubGitHub) GetLogin(context.Context) (string, error) {
-	panic("unimplemented")
-}
-
-// GetPrimaryEmail implements v1.GitHub.
-func (*StubGitHub) GetPrimaryEmail(context.Context) (string, error) {
-	panic("unimplemented")
-}
-
-func (*StubGitHub) Clone(context.Context, string, string) (*git.Repository, error) {
-	panic("unimplemented")
-}
-
-func (*StubGitHub) AddAuthToPushOptions(context.Context, *git.PushOptions) error {
-	panic("unimplemented")
-
 }
