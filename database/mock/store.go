b1b8a938b95c9057c37dcf65ef3b7b8f0db20543
Jakub Hrozek <jakub@stacklok.com>
2024-03-29 15:14:58 +0100

    Delete provider by ID without specifying project

--- database/mock/store.go
+++ database/mock/store.go
@@ -398,7 +398,7 @@ func (mr *MockStoreMockRecorder) DeleteProject(arg0, arg1 any) *gomock.Call {
 }
 
 // DeleteProvider mocks base method.
-func (m *MockStore) DeleteProvider(arg0 context.Context, arg1 db.DeleteProviderParams) error {
+func (m *MockStore) DeleteProvider(arg0 context.Context, arg1 uuid.UUID) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "DeleteProvider", arg0, arg1)
 	ret0, _ := ret[0].(error)
