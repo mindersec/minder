b17a7bd7103a4cef8c9d333fb398fe0866bc90e8
Evan Anderson <evan@stacklok.com>
2024-04-04 06:22:37 -0700

    Add tests for creating projects from GitHub Apps on login

--- internal/controlplane/handlers_user_test.go
+++ internal/controlplane/handlers_user_test.go
@@ -33,6 +33,7 @@ import (
 	"google.golang.org/grpc/metadata"
 
 	mockdb "github.com/stacklok/minder/database/mock"
+	"github.com/stacklok/minder/internal/auth"
 	mockjwt "github.com/stacklok/minder/internal/auth/mock"
 	"github.com/stacklok/minder/internal/authz/mock"
 	serverconfig "github.com/stacklok/minder/internal/config/server"
@@ -41,6 +42,7 @@ import (
 	"github.com/stacklok/minder/internal/events"
 	"github.com/stacklok/minder/internal/marketplaces"
 	"github.com/stacklok/minder/internal/providers"
+	mockprov "github.com/stacklok/minder/internal/providers/mock"
 	pb "github.com/stacklok/minder/pkg/api/protobuf/go/minder/v1"
 )
 
@@ -49,26 +51,37 @@ const (
 	tokenEndpoint = "/realms/stacklok/protocol/openid-connect/token"
 )
 
-func TestCreateUserDBMock(t *testing.T) {
+func TestCreateUser_gRPC(t *testing.T) {
 	t.Parallel()
 
-	rootID := uuid.New()
 	projectID := uuid.New()
+	keyCloakUserToken := openid.New()
+	require.NoError(t, keyCloakUserToken.Set("gh_id", "31337"))
 
 	testCases := []struct {
-		name               string
-		req                *pb.CreateUserRequest
-		buildStubs         func(store *mockdb.MockStore, jwt *mockjwt.MockJwtValidator)
+		name       string
+		req        *pb.CreateUserRequest
+		buildStubs func(ctx context.Context, store *mockdb.MockStore, jwt *mockjwt.MockJwtValidator,
+			prov *mockprov.MockProviderService) context.Context
 		checkResponse      func(t *testing.T, res *pb.CreateUserResponse, err error)
 		expectedStatusCode codes.Code
 	}{
 		{
-			name: "Success 1",
+			name: "Success",
 			req:  &pb.CreateUserRequest{},
-			buildStubs: func(store *mockdb.MockStore, jwt *mockjwt.MockJwtValidator) {
+			buildStubs: func(ctx context.Context, store *mockdb.MockStore, jwt *mockjwt.MockJwtValidator,
+				_ *mockprov.MockProviderService) context.Context {
 				tx := sql.Tx{}
 				store.EXPECT().BeginTransaction().Return(&tx, nil)
 				store.EXPECT().GetQuerierWithTransaction(gomock.Any()).Return(store)
+				store.EXPECT().
+					CreateProjectWithID(gomock.Any(), gomock.Any()).
+					Return(db.Project{
+						ID: projectID,
+						Name: "subject1",
+					}, nil)
+				store.EXPECT().CreateProvider(gomock.Any(), gomock.Any())
+
 				returnedUser := db.User{
 					ID:              1,
 					IdentitySubject: "subject1",
@@ -76,105 +89,14 @@ func TestCreateUserDBMock(t *testing.T) {
 					UpdatedAt:       time.Now(),
 				}
 				store.EXPECT().
-					CreateProjectWithID(gomock.Any(), gomock.Any()).
-					Return(db.Project{
-						ID: projectID,
-						ParentID: uuid.NullUUID{
-							UUID:  rootID,
-							Valid: true,
-						},
-					}, nil)
-				store.EXPECT().CreateProvider(gomock.Any(), gomock.Any())
-				store.EXPECT().
-					CreateUser(gomock.Any(), "subject1").
+					CreateUser(gomock.Any(), gomock.Any()).
 					Return(returnedUser, nil)
 				store.EXPECT().Commit(gomock.Any())
 				store.EXPECT().Rollback(gomock.Any())
 				tokenResult, _ := openid.NewBuilder().GivenName("Foo").FamilyName("Bar").Email("test@stacklok.com").Subject("subject1").Build()
 				jwt.EXPECT().ParseAndValidate(gomock.Any()).Return(tokenResult, nil)
-			},
-			checkResponse: func(t *testing.T, res *pb.CreateUserResponse, err error) {
-				t.Helper()
 
-				assert.NoError(t, err)
-				assert.NotNil(t, res)
-				assert.Equal(t, int32(1), res.Id)
-			},
-		},
-	}
-
-	// Create header metadata
-	md := metadata.New(map[string]string{
-		"authorization": "bearer some-access-token",
-	})
-
-	// Create a new context with added header metadata
-	ctx := metadata.NewIncomingContext(context.Background(), md)
-
-	for i := range testCases {
-		tc := testCases[i]
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-
-			mockStore := mockdb.NewMockStore(ctrl)
-			mockJwtValidator := mockjwt.NewMockJwtValidator(ctrl)
-			tc.buildStubs(mockStore, mockJwtValidator)
-			crypeng := mockcrypto.NewMockEngine(ctrl)
-
-			server := &Server{
-				store:        mockStore,
-				cfg:          &serverconfig.Config{},
-				cryptoEngine: crypeng,
-				vldtr:        mockJwtValidator,
-				authzClient:  &mock.NoopClient{Authorized: true},
-				marketplace:  marketplaces.NewNoopMarketplace(),
-			}
-
-			resp, err := server.CreateUser(ctx, tc.req)
-			tc.checkResponse(t, resp, err)
-		})
-	}
-}
-
-func TestCreateUser_gRPC(t *testing.T) {
-	t.Parallel()
-
-	projectID := uuid.New()
-
-	testCases := []struct {
-		name               string
-		req                *pb.CreateUserRequest
-		buildStubs         func(store *mockdb.MockStore, jwt *mockjwt.MockJwtValidator)
-		checkResponse      func(t *testing.T, res *pb.CreateUserResponse, err error)
-		expectedStatusCode codes.Code
-	}{
-		{
-			name: "Success",
-			req:  &pb.CreateUserRequest{},
-			buildStubs: func(store *mockdb.MockStore, jwt *mockjwt.MockJwtValidator) {
-				tx := sql.Tx{}
-				store.EXPECT().BeginTransaction().Return(&tx, nil)
-				store.EXPECT().GetQuerierWithTransaction(gomock.Any()).Return(store)
-				store.EXPECT().
-					CreateProjectWithID(gomock.Any(), gomock.Any()).
-					Return(db.Project{
-						ID: projectID,
-					}, nil)
-				store.EXPECT().CreateProvider(gomock.Any(), gomock.Any())
-				store.EXPECT().
-					CreateUser(gomock.Any(), gomock.Any()).
-					Return(db.User{
-						ID:        1,
-						CreatedAt: time.Now(),
-						UpdatedAt: time.Now(),
-					}, nil).
-					Times(1)
-				store.EXPECT().Commit(gomock.Any())
-				store.EXPECT().Rollback(gomock.Any())
-				jwt.EXPECT().ParseAndValidate(gomock.Any()).Return(openid.New(), nil)
+				return ctx
 			},
 			checkResponse: func(t *testing.T, res *pb.CreateUserResponse, err error) {
 				t.Helper()
@@ -183,6 +105,132 @@ func TestCreateUser_gRPC(t *testing.T) {
 				assert.NotNil(t, res)
 				assert.Equal(t, int32(1), res.Id)
 				assert.Equal(t, projectID.String(), res.ProjectId)
+				assert.Equal(t, "subject1", res.ProjectName)
+				assert.NotNil(t, res.CreatedAt)
+			},
+			expectedStatusCode: codes.OK,
+		},
+		{
+			name: "Success with pending App",
+			req:  &pb.CreateUserRequest{},
+			buildStubs: func(ctx context.Context, store *mockdb.MockStore, jwt *mockjwt.MockJwtValidator,
+				prov *mockprov.MockProviderService) context.Context {
+				ctx = auth.WithAuthTokenContext(ctx, keyCloakUserToken)
+
+				tx := sql.Tx{}
+				store.EXPECT().BeginTransaction().Return(&tx, nil)
+				store.EXPECT().GetQuerierWithTransaction(gomock.Any()).Return(store)
+
+				returnedUser := db.User{
+					ID:              1,
+					IdentitySubject: "subject1",
+					CreatedAt:       time.Now(),
+					UpdatedAt:       time.Now(),
+				}
+				store.EXPECT().
+					CreateUser(gomock.Any(), gomock.Any()).
+					Return(returnedUser, nil)
+
+				store.EXPECT().
+					GetUnclaimedInstallationsByUser(gomock.Any(), sql.NullString{String: "31337", Valid: true}).
+					Return([]db.ProviderGithubAppInstallation{
+						{
+							AppInstallationID: 10,
+							OrganizationID:    9000,
+							EnrollingUserID:   sql.NullString{String: "31337", Valid: true},
+						},
+					}, nil)
+
+				prov.EXPECT().
+					CreateGitHubAppWithoutInvitation(gomock.Any(), gomock.Any(), int64(31337), int64(10)).
+					Return(&db.Project{
+						ID:   projectID,
+						Name: "github-org1",
+					}, nil)
+
+				store.EXPECT().Commit(gomock.Any())
+				store.EXPECT().Rollback(gomock.Any())
+				tokenResult, _ := openid.NewBuilder().GivenName("Foo").FamilyName("Bar").Email("test@stacklok.com").Subject("subject1").Build()
+				jwt.EXPECT().ParseAndValidate(gomock.Any()).Return(tokenResult, nil)
+
+				return ctx
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateUserResponse, err error) {
+				t.Helper()
+
+				assert.NoError(t, err)
+				assert.NotNil(t, res)
+				assert.Equal(t, int32(1), res.Id)
+				assert.Equal(t, projectID.String(), res.ProjectId)
+				assert.Equal(t, "github-org1", res.ProjectName)
+				assert.NotNil(t, res.CreatedAt)
+			},
+			expectedStatusCode: codes.OK,
+		},
+		{
+			name: "Success with two pending Apps",
+			req:  &pb.CreateUserRequest{},
+			buildStubs: func(ctx context.Context, store *mockdb.MockStore, jwt *mockjwt.MockJwtValidator,
+				prov *mockprov.MockProviderService) context.Context {
+				ctx = auth.WithAuthTokenContext(ctx, keyCloakUserToken)
+
+				tx := sql.Tx{}
+				store.EXPECT().BeginTransaction().Return(&tx, nil)
+				store.EXPECT().GetQuerierWithTransaction(gomock.Any()).Return(store)
+
+				returnedUser := db.User{
+					ID:              1,
+					IdentitySubject: "subject1",
+					CreatedAt:       time.Now(),
+					UpdatedAt:       time.Now(),
+				}
+				store.EXPECT().
+					CreateUser(gomock.Any(), gomock.Any()).
+					Return(returnedUser, nil)
+
+				store.EXPECT().
+					GetUnclaimedInstallationsByUser(gomock.Any(), sql.NullString{String: "31337", Valid: true}).
+					Return([]db.ProviderGithubAppInstallation{
+						{
+							AppInstallationID: 10,
+							OrganizationID:    9000,
+							EnrollingUserID:   sql.NullString{String: "31337", Valid: true},
+						}, {
+							AppInstallationID: 11,
+							OrganizationID:    9001,
+							EnrollingUserID:   sql.NullString{String: "31337", Valid: true},
+						},
+					}, nil)
+
+				prov.EXPECT().
+					CreateGitHubAppWithoutInvitation(gomock.Any(), gomock.Any(), int64(31337), int64(10)).
+					Return(&db.Project{
+						ID:   projectID,
+						Name: "github-org1",
+					}, nil)
+
+				prov.EXPECT().
+					CreateGitHubAppWithoutInvitation(gomock.Any(), gomock.Any(), int64(31337), int64(11)).
+					Return(&db.Project{
+						ID:   uuid.New(),
+						Name: "github-org2",
+					}, nil)
+
+				store.EXPECT().Commit(gomock.Any())
+				store.EXPECT().Rollback(gomock.Any())
+				tokenResult, _ := openid.NewBuilder().GivenName("Foo").FamilyName("Bar").Email("test@stacklok.com").Subject("subject1").Build()
+				jwt.EXPECT().ParseAndValidate(gomock.Any()).Return(tokenResult, nil)
+
+				return ctx
+			},
+			checkResponse: func(t *testing.T, res *pb.CreateUserResponse, err error) {
+				t.Helper()
+
+				assert.NoError(t, err)
+				assert.NotNil(t, res)
+				assert.Equal(t, int32(1), res.Id)
+				assert.Equal(t, projectID.String(), res.ProjectId)
+				assert.Equal(t, "github-org1", res.ProjectName)
 				assert.NotNil(t, res.CreatedAt)
 			},
 			expectedStatusCode: codes.OK,
@@ -207,20 +255,28 @@ func TestCreateUser_gRPC(t *testing.T) {
 
 			mockStore := mockdb.NewMockStore(ctrl)
 			mockJwtValidator := mockjwt.NewMockJwtValidator(ctrl)
-			tc.buildStubs(mockStore, mockJwtValidator)
-			evt, err := events.Setup(context.Background(), &serverconfig.EventConfig{
-				Driver:    "go-channel",
-				GoChannel: serverconfig.GoChannelEventConfig{},
-			})
-			require.NoError(t, err, "failed to setup eventer")
-			server, err := NewServer(mockStore, evt, &serverconfig.Config{
-				Auth: serverconfig.AuthConfig{
-					TokenKey: generateTokenKey(t),
-				},
-			}, mockJwtValidator, providers.NewProviderStore(mockStore))
-			require.NoError(t, err, "failed to create test server")
+			mockProviders := mockprov.NewMockProviderService(ctrl)
+			reqCtx := tc.buildStubs(ctx, mockStore, mockJwtValidator, mockProviders)
+			crypeng := mockcrypto.NewMockEngine(ctrl)
 
-			resp, err := server.CreateUser(ctx, tc.req)
+			server := &Server{
+				store:        mockStore,
+				cfg:          &serverconfig.Config{},
+				cryptoEngine: crypeng,
+				vldtr:        mockJwtValidator,
+				providers:    mockProviders,
+				authzClient:  &mock.NoopClient{Authorized: true},
+				marketplace:  marketplaces.NewNoopMarketplace(),
+			}
+
+			// server, err := NewServer(mockStore, evt, &serverconfig.Config{
+			// 	Auth: serverconfig.AuthConfig{
+			// 		TokenKey: generateTokenKey(t),
+			// 	},
+			// }, mockJwtValidator, providers.NewProviderStore(mockStore))
+			// require.NoError(t, err, "failed to create test server")
+
+			resp, err := server.CreateUser(reqCtx, tc.req)
 			tc.checkResponse(t, resp, err)
 		})
 	}
