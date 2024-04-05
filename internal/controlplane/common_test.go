8965deca79f415154b467c7c023a45fb14b1c14a
Adolfo García Veytia (Puerco) <puerco@stacklok.com>
2024-04-05 12:42:28 -0600

    Add test for GetRemediationURLFromMetadata
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@stacklok.com>

--- internal/controlplane/common_test.go
+++ internal/controlplane/common_test.go
@@ -20,6 +20,39 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestGetRemediationURLFromMetadata(t *testing.T) {
+	validData := []byte(`{"pr_number": 18}`)
+	t.Parallel()
+	for _, tc := range []struct {
+		name        string
+		data        []byte
+		repo        string
+		expectedURL string
+		mustErr     bool
+	}{
+		{"normal", validData, "My-Example_1.0/Test_2", "https://github.com/My-Example_1.0/Test_2/pull/18", false},
+		{"invalid-slug", validData, "example", "", true},
+		{"no-pr", []byte(`{}`), "example/test", "", false},
+		{"no-slug", validData, "", "", true},
+		{"no-slug-no-pr", []byte(`{}`), "", "", false},
+		{"invalid-json", []byte(`Yo!`), "", "", true},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			url, err := getRemediationURLFromMetadata(tc.data, tc.repo)
+			if tc.mustErr {
+				require.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.expectedURL, url)
+		})
+	}
+
+}
+
 func TestGetAlertURLFromMetadata(t *testing.T) {
 	t.Parallel()
 	validPayload := []byte(`{"ghsa_id": "GHAS-advisory_ID_here"}`)
