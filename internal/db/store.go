32357b343206ac10113d0d1601d52bd93854d66a
Don Browne <dmjb@users.noreply.github.com>
2024-04-18 10:23:50 +0100

    Implement `CanImplement` method for providers (#3115)
    Provide `CanImplement` method for providers
    
    Relates to #2845
    
    Allows both the provider DB record as well as instances of the Provider
    interface to be queried to determine if they support a particular trait.
    Will be used more in future PRs.

--- internal/db/store.go
+++ internal/db/store.go
@@ -19,10 +19,8 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"strings"
 
 	"github.com/google/uuid"
-	"github.com/sqlc-dev/pqtype"
 )
 
 // ExtendQuerier extends the Querier interface with custom queries
@@ -132,61 +130,4 @@ func WithTransaction[T any](store Store, fn func(querier ExtendQuerier) (T, erro
 		return result, err
 	}
 	return result, store.Commit(tx)
-}
-
-// ProfileRow is an interface row in the profiles table
-type ProfileRow interface {
-	GetProfile() Profile
-	GetEntityProfile() NullEntities
-	GetContextualRules() pqtype.NullRawMessage
-}
-
-// GetProfile returns the profile
-func (r ListProfilesByProjectIDAndLabelRow) GetProfile() Profile {
-	return r.Profile
-}
-
-// GetEntityProfile returns the entity profile
-func (r ListProfilesByProjectIDAndLabelRow) GetEntityProfile() NullEntities {
-	return r.ProfilesWithEntityProfile.Entity
-}
-
-// GetContextualRules returns the contextual rules
-func (r ListProfilesByProjectIDAndLabelRow) GetContextualRules() pqtype.NullRawMessage {
-	return r.ProfilesWithEntityProfile.ContextualRules
-}
-
-// GetProfile returns the profile
-func (r ListProfilesByProjectIDRow) GetProfile() Profile {
-	return r.Profile
-}
-
-// GetEntityProfile returns the entity profile
-func (r ListProfilesByProjectIDRow) GetEntityProfile() NullEntities {
-	return r.ProfilesWithEntityProfile.Entity
-}
-
-// GetContextualRules returns the contextual rules
-func (r ListProfilesByProjectIDRow) GetContextualRules() pqtype.NullRawMessage {
-	return r.ProfilesWithEntityProfile.ContextualRules
-}
-
-// LabelsFromFilter parses the filter string and populates the IncludeLabels and ExcludeLabels fields
-func (lp *ListProfilesByProjectIDAndLabelParams) LabelsFromFilter(filter string) {
-	// otherwise Split would have returned a slice with one empty string
-	if filter == "" {
-		return
-	}
-
-	for _, label := range strings.Split(filter, ",") {
-		switch {
-		case label == "*":
-			lp.IncludeLabels = append(lp.IncludeLabels, label)
-		case strings.HasPrefix(label, "!"):
-			// if the label starts with a "!", it is a negative filter, add it to the negative list
-			lp.ExcludeLabels = append(lp.ExcludeLabels, label[1:])
-		default:
-			lp.IncludeLabels = append(lp.IncludeLabels, label)
-		}
-	}
 }
