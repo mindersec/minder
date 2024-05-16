2960df2ff831af094c52afddb9f2ea7a6ed6eb76
Don Browne <don@stacklok.com>
2024-05-16 13:11:26 +0100

    drop and recreate column

--- database/migrations/000057_encrypted_redirect_jsonb.down.sql
+++ database/migrations/000057_encrypted_redirect_jsonb.down.sql
@@ -14,4 +14,9 @@
 
 -- add columns for new encrypted data format
 
-ALTER TABLE session_store ALTER COLUMN encrypted_redirect TYPE TEXT;
+-- This column is not used at this point in time, it is always NULL.
+BEGIN;
+-- can't cast between TEXT and JSONB, drop and recreate
+ALTER TABLE session_store DROP COLUMN encrypted_redirect;
+ALTER TABLE session_store ADD COLUMN encrypted_redirect TEXT;
+COMMIT;
