54ca1ca328cc473ecdfcf3b130a5bf8f853a7c82
Adam Korczynski <adam@adalogics.com>
2024-05-25 18:22:31 +0100

    Add fuzz test for diff ingester parser
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

---
+++ internal/engine/ingester/diff/fuzz_test.go
@@ -0,0 +1,35 @@
+// Copyright 2024 Stacklok, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package diff
+
+import (
+	"testing"
+)
+
+func FuzzDiffParse(f *testing.F) {
+	f.Fuzz(func(t *testing.T, param string, parser int) {
+		switch parser % 3 {
+		case 0:
+			//nolint:gosec
+			requirementsParse(param)
+		case 1:
+			//nolint:gosec
+			npmParse(param)
+		case 2:
+			//nolint:gosec
+			goParse(param)
+		}
+	})
+}
