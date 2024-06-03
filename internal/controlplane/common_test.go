f63b6c87b1217c7728039a4a5704041996e3a807
Juan Antonio Osorio <ozz@stacklok.com>
2024-06-03 13:42:18 +0300

    Add ContextV2 protobuf structure
    This takes a `project_id` by default. It also provides the functionality
    to prefer a `ContextV2` from a request if it's present and available.
    
    Signed-off-by: Juan Antonio Osorio <ozz@stacklok.com>

--- internal/controlplane/common_test.go
+++ internal/controlplane/common_test.go
@@ -17,7 +17,12 @@ package controlplane
 import (
 	"testing"
 
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/stacklok/minder/internal/util/ptr"
+	pb "github.com/stacklok/minder/pkg/api/protobuf/go/minder/v1"
 )
 
 func TestGetRemediationURLFromMetadata(t *testing.T) {
@@ -80,4 +85,151 @@ func TestGetAlertURLFromMetadata(t *testing.T) {
 			require.Equal(t, tc.expected, res)
 		})
 	}
+}
+
+func Test_getProjectFromContextV2(t *testing.T) {
+	t.Parallel()
+
+	proj1 := uuid.New()
+	proj2 := uuid.New()
+
+	type args struct {
+		accessor HasProtoContextV2Compat
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    uuid.UUID
+		wantErr bool
+	}{
+		{
+			name: "no project",
+			args: args{
+				accessor: newMockHasProtoContextV2(),
+			},
+			want:    uuid.Nil,
+			wantErr: true,
+		},
+		{
+			name: "v1 project",
+			args: args{
+				accessor: newMockHasProtoContextV2().withV1(&pb.Context{
+					Project: ptr.Ptr(proj1.String()),
+				}),
+			},
+			want:    proj1,
+			wantErr: false,
+		},
+		{
+			name: "v2 project",
+			args: args{
+				accessor: newMockHasProtoContextV2().withV2(&pb.ContextV2{
+					ProjectId: proj1.String(),
+				}),
+			},
+			want:    proj1,
+			wantErr: false,
+		},
+		{
+			name: "v2 project wins",
+			args: args{
+				accessor: newMockHasProtoContextV2().withV1(&pb.Context{
+					Project: ptr.Ptr(proj1.String()),
+				}).withV2(&pb.ContextV2{
+					ProjectId: proj2.String(),
+				}),
+			},
+			want:    proj2,
+			wantErr: false,
+		},
+		{
+			name: "v2 project wins with malformed v1",
+			args: args{
+				accessor: newMockHasProtoContextV2().withV1(&pb.Context{
+					Project: ptr.Ptr("malformed"),
+				}).withV2(&pb.ContextV2{
+					ProjectId: proj2.String(),
+				}),
+			},
+			want:    proj2,
+			wantErr: false,
+		},
+		{
+			name: "malformed v2 project",
+			args: args{
+				accessor: newMockHasProtoContextV2().withV2(&pb.ContextV2{
+					ProjectId: "malformed",
+				}),
+			},
+			want:    uuid.Nil,
+			wantErr: true,
+		},
+		{
+			name: "malformed v2 project is still an error",
+			args: args{
+				accessor: newMockHasProtoContextV2().withV1(&pb.Context{
+					Project: ptr.Ptr(proj1.String()),
+				}).withV2(&pb.ContextV2{
+					ProjectId: "malformed",
+				}),
+			},
+			want:    uuid.Nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := getProjectFromContextV2Compat(tt.args.accessor)
+			if tt.wantErr {
+				assert.Error(t, err, "expected error")
+				return
+			}
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+type mockHasProtoContextV2 struct {
+	getV1 func() *pb.Context
+	getV2 func() *pb.ContextV2
+}
+
+func emptyv1() *pb.Context {
+	return nil
+}
+
+func emptyv2() *pb.ContextV2 {
+	return nil
+}
+
+func newMockHasProtoContextV2() *mockHasProtoContextV2 {
+	return &mockHasProtoContextV2{
+		getV1: emptyv1,
+		getV2: emptyv2,
+	}
+}
+
+func (m *mockHasProtoContextV2) withV1(v1 *pb.Context) *mockHasProtoContextV2 {
+	m.getV1 = func() *pb.Context {
+		return v1
+	}
+	return m
+}
+
+func (m *mockHasProtoContextV2) withV2(v2 *pb.ContextV2) *mockHasProtoContextV2 {
+	m.getV2 = func() *pb.ContextV2 {
+		return v2
+	}
+	return m
+}
+
+func (m *mockHasProtoContextV2) GetContext() *pb.Context {
+	return m.getV1()
+}
+
+func (m *mockHasProtoContextV2) GetContextV2() *pb.ContextV2 {
+	return m.getV2()
 }
