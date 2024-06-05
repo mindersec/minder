4689b1e88fa10c24911960aed55ddf8b24c600cb
Radoslav Dimitrov <radoslav@stacklok.com>
2024-06-05 12:02:06 +0300

    Add database migration for the user_invites table
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>

---
+++ database/migrations/000061_user_invites.down.sql
@@ -0,0 +1,30 @@
+-- Copyright 2024 Stacklok, Inc
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+BEGIN;
+
+-- drop the indexes
+DROP INDEX IF EXISTS idx_user_invites_email;
+DROP INDEX IF EXISTS idx_user_invites_project;
+DROP INDEX IF EXISTS idx_user_invites_invitee;
+DROP INDEX IF EXISTS idx_user_invites_invited_by;
+
+-- drop the table
+DROP TABLE IF EXISTS user_invites;
+
+-- drop the types
+DROP TYPE IF EXISTS invite_status;
+DROP TYPE IF EXISTS user_role;
+
+COMMIT;
---
+++ database/migrations/000061_user_invites.up.sql
@@ -0,0 +1,48 @@
+-- Copyright 2024 Stacklok, Inc
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+BEGIN;
+
+-- describes the invite status
+CREATE TYPE invite_status AS ENUM ('pending', 'accepted', 'declined', 'expired', 'revoked');
+-- reflects the roles we have in OpenFGA
+CREATE TYPE user_role AS ENUM ('admin', 'editor', 'viewer', 'policy_writer', 'permissions_manager');
+
+CREATE TABLE IF NOT EXISTS user_invites(
+    id           UUID NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+    email        TEXT NOT NULL,
+    status       invite_status NOT NULL DEFAULT 'pending',
+    role         user_role NOT NULL DEFAULT 'viewer',
+    project      UUID NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
+    invitee      INTEGER NULL REFERENCES users(id) ON DELETE CASCADE,
+    invited_by   INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    created_at   TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at   TIMESTAMP NOT NULL DEFAULT NOW(),
+    -- ensure there's one invite for this email per project
+    UNIQUE (email, project)
+);
+
+-- create an index on the email column
+CREATE INDEX idx_user_invites_email ON user_invites(email);
+
+-- create an index on the project column
+CREATE INDEX idx_user_invites_project ON user_invites(project);
+
+-- create an index on the invitee column
+CREATE INDEX idx_user_invites_invitee ON user_invites(invitee);
+
+-- create an index on the invited_by column
+CREATE INDEX idx_user_invites_invited_by ON user_invites(invited_by);
+
+COMMIT;
