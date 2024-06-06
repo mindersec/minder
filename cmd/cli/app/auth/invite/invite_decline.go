5fe4f11e8844b6d92ad9fe5440e4844851f7adc1
Radoslav Dimitrov <radoslav@stacklok.com>
2024-06-06 10:42:56 +0300

    Implement minder auth invite CLI commands
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>

---
+++ cmd/cli/app/auth/invite/invite_decline.go
@@ -0,0 +1,59 @@
+//
+// Copyright 2023 Stacklok, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package invite provides the auth invite command for the minder CLI.
+package invite
+
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+
+	"github.com/stacklok/minder/internal/util/cli"
+	minderv1 "github.com/stacklok/minder/pkg/api/protobuf/go/minder/v1"
+)
+
+// inviteDeclineCmd represents the decline command
+var inviteDeclineCmd = &cobra.Command{
+	Use:   "decline",
+	Short: "Declines a pending invitation",
+	Long:  `Declines a pending invitation for the current minder user`,
+	RunE:  cli.GRPCClientWrapRunE(inviteDeclineCommand),
+	Args:  cobra.ExactArgs(1),
+}
+
+// inviteAcceptCommand is the whoami subcommand
+func inviteDeclineCommand(ctx context.Context, cmd *cobra.Command, args []string, conn *grpc.ClientConn) error {
+	client := minderv1.NewUserServiceClient(conn)
+	code := args[0]
+	// No longer print usage on returned error, since we've parsed our inputs
+	// See https://github.com/spf13/cobra/issues/340#issuecomment-374617413
+	cmd.SilenceUsage = true
+
+	res, err := client.ResolveInvitation(ctx, &minderv1.ResolveInvitationRequest{
+		Accept: false,
+		Code:   code,
+	})
+	if err != nil {
+		return cli.MessageAndError("Error resolving invitation", err)
+	}
+	cmd.Printf("Invitation %s for %s to become %s of project %s was declined!\n", code, res.Email, res.Role, res.Project)
+	return nil
+}
+
+func init() {
+	inviteCmd.AddCommand(inviteDeclineCmd)
+}
---
+++ cmd/cli/app/auth/invite/invite_list.go
@@ -0,0 +1,89 @@
+//
+// Copyright 2023 Stacklok, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package invite provides the auth invite command for the minder CLI.
+package invite
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+
+	"github.com/stacklok/minder/cmd/cli/app"
+	"github.com/stacklok/minder/internal/util"
+	"github.com/stacklok/minder/internal/util/cli"
+	"github.com/stacklok/minder/internal/util/cli/table"
+	"github.com/stacklok/minder/internal/util/cli/table/layouts"
+	minderv1 "github.com/stacklok/minder/pkg/api/protobuf/go/minder/v1"
+)
+
+// inviteListCmd represents the list command
+var inviteListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List pending invitations",
+	Long:  `List shows all pending invitations for the current minder user`,
+	RunE:  cli.GRPCClientWrapRunE(inviteListCommand),
+}
+
+// inviteListCommand is the whoami subcommand
+func inviteListCommand(ctx context.Context, cmd *cobra.Command, _ []string, conn *grpc.ClientConn) error {
+	client := minderv1.NewUserServiceClient(conn)
+
+	// No longer print usage on returned error, since we've parsed our inputs
+	// See https://github.com/spf13/cobra/issues/340#issuecomment-374617413
+	cmd.SilenceUsage = true
+
+	format := viper.GetString("output")
+
+	res, err := client.ListInvitations(ctx, &minderv1.ListInvitationsRequest{})
+	if err != nil {
+		return cli.MessageAndError("Error listing invitations", err)
+	}
+
+	switch format {
+	case app.JSON:
+		out, err := util.GetJsonFromProto(res)
+		if err != nil {
+			return cli.MessageAndError("Error getting json from proto", err)
+		}
+		cmd.Println(out)
+	case app.YAML:
+		out, err := util.GetYamlFromProto(res)
+		if err != nil {
+			return cli.MessageAndError("Error getting yaml from proto", err)
+		}
+		cmd.Println(out)
+	case app.Table:
+		t := table.New(table.Simple, layouts.Default, []string{"Sponsor", "Project", "Role", "Code"})
+		for _, v := range res.Invitations {
+			t.AddRow(v.SponsorDisplay, v.Project, v.Role, v.Code)
+		}
+		t.Render()
+	default:
+		return fmt.Errorf("unsupported output format: %s", format)
+	}
+	return nil
+}
+
+func init() {
+	inviteCmd.AddCommand(inviteListCmd)
+
+	inviteListCmd.Flags().StringP("output", "o", app.Table,
+		fmt.Sprintf("Output format (one of %s)", strings.Join(app.SupportedOutputFormats(), ",")))
+}
