74b17cffc17a7c433774b06ce5d271de20a8dd87
Radoslav Dimitrov <radoslav@stacklok.com>
2024-06-07 09:15:25 +0300

    Minder CLI - New commands for auth invite - list, accept <code> and decline <code> (#3551)
    * Implement minder auth invite CLI commands
    
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>
    
    * Add expiration to the list output cli command
    
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>
    
    * Hide the auth invite commands until it's fully implemented in the server
    
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>
    
    * Update cmd/cli/app/auth/invite/invite.go
    
    Co-authored-by: Evan Anderson <evan@stacklok.com>
    
    * Fix missing comma
    
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>
    
    ---------
    
    Signed-off-by: Radoslav Dimitrov <radoslav@stacklok.com>
    Co-authored-by: Evan Anderson <evan@stacklok.com>

---
+++ cmd/cli/app/auth/invite/invite_accept.go
@@ -0,0 +1,60 @@
+//
+// Copyright 2023 Stacklok, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package invite provides the auth invite command for the minder CLI.
+package invite
+
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+
+	"github.com/stacklok/minder/internal/util/cli"
+	minderv1 "github.com/stacklok/minder/pkg/api/protobuf/go/minder/v1"
+)
+
+// inviteAcceptCmd represents the accept command
+var inviteAcceptCmd = &cobra.Command{
+	Hidden: true, // TODO: This hides the command, remove it once it's implemented
+	Use:    "accept",
+	Short:  "Accept a pending invitation",
+	Long:   `Accept a pending invitation for the current minder user`,
+	RunE:   cli.GRPCClientWrapRunE(inviteAcceptCommand),
+	Args:   cobra.ExactArgs(1),
+}
+
+// inviteAcceptCommand is the whoami subcommand
+func inviteAcceptCommand(ctx context.Context, cmd *cobra.Command, args []string, conn *grpc.ClientConn) error {
+	client := minderv1.NewUserServiceClient(conn)
+	code := args[0]
+	// No longer print usage on returned error, since we've parsed our inputs
+	// See https://github.com/spf13/cobra/issues/340#issuecomment-374617413
+	cmd.SilenceUsage = true
+
+	res, err := client.ResolveInvitation(ctx, &minderv1.ResolveInvitationRequest{
+		Accept: true,
+		Code:   code,
+	})
+	if err != nil {
+		return cli.MessageAndError("Error resolving invitation", err)
+	}
+	cmd.Printf("Invitation %s for %s to become %s of project %s was accepted!\n", code, res.Email, res.Role, res.Project)
+	return nil
+}
+
+func init() {
+	inviteCmd.AddCommand(inviteAcceptCmd)
+}
