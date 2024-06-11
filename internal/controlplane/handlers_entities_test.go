f9236fc8efce6411f85af94740d986fc4b70e0e6
Teodor Yanev <teodor@stacklok.com>
2024-06-11 21:30:14 +0300

    update: add tests and update path

---
+++ internal/controlplane/handlers_entities_test.go
@@ -0,0 +1,118 @@
+package controlplane
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+
+	"github.com/stacklok/minder/internal/db"
+	"github.com/stacklok/minder/internal/engine"
+	mockevents "github.com/stacklok/minder/internal/events/mock"
+	mockgh "github.com/stacklok/minder/internal/providers/github/mock"
+	mockmanager "github.com/stacklok/minder/internal/providers/manager/mock"
+	rf "github.com/stacklok/minder/internal/repositories/github/mock/fixtures"
+	pb "github.com/stacklok/minder/pkg/api/protobuf/go/minder/v1"
+	provinfv1 "github.com/stacklok/minder/pkg/providers/v1"
+)
+
+func TestServer_ReconcileEntityRegistration(t *testing.T) {
+	t.Parallel()
+
+	scenarios := []struct {
+		Name             string
+		RepoServiceSetup repoMockBuilder
+		GitHubSetup      githubMockBuilder
+		ProviderSetup    func(ctrl *gomock.Controller) *mockmanager.MockProviderManager
+		EventerSetup     func(ctrl *gomock.Controller) *mockevents.MockInterface
+		ProviderFails    bool
+		ExpectedResults  []*pb.UpstreamRepositoryRef
+		ExpectedError    string
+	}{
+		{
+			Name:        "[positive] successful reconciliation",
+			GitHubSetup: newGitHub(withSuccessfulListAllRepositories),
+			RepoServiceSetup: rf.NewRepoService(
+				rf.WithSuccessfulListRepositories(
+					simpleDbRepository(repoName, remoteRepoId),
+				),
+			),
+			ProviderSetup: func(ctrl *gomock.Controller) *mockmanager.MockProviderManager {
+				providerManager := mockmanager.NewMockProviderManager(ctrl)
+				providerManager.EXPECT().BulkInstantiateByTrait(
+					gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(),
+				).Return(map[string]provinfv1.Provider{provider.Name: mockgh.NewMockGitHub(ctrl)}, []string{}, nil).Times(1)
+				return providerManager
+			},
+			EventerSetup: func(ctrl *gomock.Controller) *mockevents.MockInterface {
+				events := mockevents.NewMockInterface(ctrl)
+				events.EXPECT().Publish(gomock.Any(), gomock.Any()).Times(1)
+				return events
+			},
+		},
+		{
+			Name:        "[negative] failed to list repositories",
+			GitHubSetup: newGitHub(withFailedListAllRepositories(errDefault)),
+			RepoServiceSetup: rf.NewRepoService(
+				rf.WithSuccessfulListRepositories(
+					simpleDbRepository(repoName, remoteRepoId),
+				),
+			),
+			ProviderSetup: func(ctrl *gomock.Controller) *mockmanager.MockProviderManager {
+				providerManager := mockmanager.NewMockProviderManager(ctrl)
+				providerManager.EXPECT().BulkInstantiateByTrait(
+					gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(),
+				).Return(map[string]provinfv1.Provider{provider.Name: mockgh.NewMockGitHub(ctrl)}, []string{}, nil).Times(1)
+				return providerManager
+			},
+			ExpectedError: "cannot register entities for providers: [github]",
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.Name, func(t *testing.T) {
+			t.Parallel()
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			ctx := engine.WithEntityContext(context.Background(), &engine.EntityContext{
+				Project: engine.Project{ID: projectID},
+			})
+
+			prov := scenario.GitHubSetup(ctrl)
+			manager := mockmanager.NewMockProviderManager(ctrl)
+			manager.EXPECT().BulkInstantiateByTrait(
+				gomock.Any(),
+				gomock.Eq(projectID),
+				gomock.Eq(db.ProviderTypeRepoLister),
+				gomock.Eq(""),
+			).Return(map[string]provinfv1.Provider{provider.Name: prov}, []string{}, nil)
+
+			server := createServer(
+				ctrl,
+				scenario.RepoServiceSetup,
+				scenario.ProviderFails,
+				manager,
+			)
+
+			if scenario.EventerSetup != nil {
+				server.evt = scenario.EventerSetup(ctrl)
+			}
+
+			projectIDStr := projectID.String()
+			req := &pb.ReconcileEntityRegistrationRequest{
+				Context: &pb.Context{
+					Project: &projectIDStr,
+				},
+			}
+			res, err := server.ReconcileEntityRegistration(ctx, req)
+			if scenario.ExpectedError == "" {
+				require.NoError(t, err)
+			} else {
+				require.Nil(t, res)
+				require.Contains(t, err.Error(), scenario.ExpectedError)
+			}
+		})
+	}
+}
