aa40ff6973678d8f141faebf818808dc9382f1b5
Jakub Hrozek <jakub@stacklok.com>
2024-06-26 20:47:44 +0200

    Add DB methods for selector management
    Adds the needed SQL methods to manage profile selectors
    
    Fixes: #3720

---
+++ database/query/selectors.sql
@@ -0,0 +1,24 @@
+-- name: CreateSelector :one
+INSERT INTO profile_selectors (profile_id, entity, selector, comment)
+VALUES ($1, $2, $3, $4)
+RETURNING id, profile_id, entity, selector, comment;
+
+-- name: GetSelectorsByProfileID :many
+SELECT id, profile_id, entity, selector, comment
+FROM profile_selectors
+WHERE profile_id = $1;
+
+-- name: UpdateSelector :one
+UPDATE profile_selectors
+SET entity = $2, selector = $3, comment = $4
+WHERE id = $1
+RETURNING id, profile_id, entity, selector, comment;
+
+-- name: DeleteSelector :exec
+DELETE FROM profile_selectors
+WHERE id = $1;
+
+-- name: GetSelectorByID :one
+SELECT id, profile_id, entity, selector, comment
+FROM profile_selectors
+WHERE id = $1;
