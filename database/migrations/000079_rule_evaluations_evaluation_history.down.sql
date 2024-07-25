34e12f554c2d08c64efcbbdbc075999dbf4b023a
Don Browne <don@stacklok.com>
2024-07-25 13:26:00 +0100

    update down script

--- database/migrations/000079_rule_evaluations_evaluation_history.down.sql
+++ database/migrations/000079_rule_evaluations_evaluation_history.down.sql
@@ -15,6 +15,7 @@
 BEGIN;
 
 ALTER TABLE public.rule_evaluations DROP COLUMN rule_entity_id;
+ALTER TABLE public.rule_evaluations DROP COLUMN rule_instance_id;
 
 ALTER TABLE latest_evaluation_statuses DROP CONSTRAINT latest_evaluation_statuses_profile_id_fkey;
 ALTER TABLE latest_evaluation_statuses
