e9c12a7c72114ccec991a3cb3e4675c2d8fb5edb
Don Browne <don@stacklok.com>
2024-08-15 17:35:02 +0100

    Drop rule evaluations table
    These tables are no longer used.

---
+++ database/migrations/000094_rule_evaluation_drop.up.sql
@@ -0,0 +1,23 @@
+-- Copyright 2024 Stacklok, Inc
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+BEGIN;
+
+-- Drop these tables, they have been replaced by evaluation_rule_entities and the related tables.
+DROP TABLE rule_details_eval;
+DROP TABLE rule_details_alert;
+DROP TABLE rule_details_remediate;
+DROP TABLE rule_evaluations;
+
+COMMIT;
