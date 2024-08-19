d9791557f2ffba67f4cbd7a8ab68deb57f50f134
Radoslav Dimitrov <radoslav@stacklok.com>
2024-08-19 10:26:36 +0300

    Revert "Check for non-empty length of previous metadata (#4182)"
    This reverts commit 2fddec58efaef2e862496defcf6d1c9e125b7302.

--- internal/engine/actions/alert/security_advisory/security_advisory.go
+++ internal/engine/actions/alert/security_advisory/security_advisory.go
@@ -336,7 +336,7 @@ func (alert *Alert) getParamsForSecurityAdvisory(
 		},
 	}
 	// Unmarshal the existing alert metadata, if any
-	if metadata != nil && len(*metadata) != 0 {
+	if metadata != nil {
 		meta := &alertMetadata{}
 		err := json.Unmarshal(*metadata, meta)
 		if err != nil {
