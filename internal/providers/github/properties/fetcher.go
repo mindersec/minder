b1a085dc909bf8462146f0a1e693538568e6aed0
Jakub Hrozek <jakub@stacklok.com>
2024-09-02 10:39:29 +0200

    Slight generalization of the GH property fetching
    The implementation of FetchProperty and FetchAllProperties for GitHub
    was not generic enough as it required that the properties contain the
    `name` key. For some properties, like pull request, the provider must
    fetch by other keys, so let's just pass the properties to the wrapper
    and let the wrapper error out if the get-by-properties don't contain the
    necessary data for fetching the property.
    
    Also moves the propertyOrigin struct to the generic fetcher.go source to
    make it clearer that it's to be reused by other entities.

--- internal/providers/github/properties/fetcher.go
+++ internal/providers/github/properties/fetcher.go
@@ -18,7 +18,6 @@ package properties
 
 import (
 	"context"
-	"fmt"
 
 	go_github "github.com/google/go-github/v63/github"
 
@@ -27,14 +26,20 @@ import (
 )
 
 // GhPropertyWrapper is a function that fetches a property from the GitHub API
-type GhPropertyWrapper func(ctx context.Context, ghCli *go_github.Client, name string) (map[string]any, error)
+type GhPropertyWrapper func(
+	ctx context.Context, ghCli *go_github.Client, lookupProperties *properties.Properties,
+) (map[string]any, error)
 
 // GhPropertyFetcher is an interface for fetching properties from the GitHub API
 type GhPropertyFetcher interface {
+	ghPropertyFetcherBase
+	GetName(props *properties.Properties) (string, error)
+}
+
+type ghPropertyFetcherBase interface {
 	WrapperForProperty(propertyKey string) GhPropertyWrapper
 	AllPropertyWrappers() []GhPropertyWrapper
 	OperationalProperties() []string
-	GetName(props *properties.Properties) (string, error)
 }
 
 // GhPropertyFetcherFactory is an interface for creating GhPropertyFetcher instances
@@ -51,56 +56,52 @@ func NewPropertyFetcherFactory() GhPropertyFetcherFactory {
 
 // EntityPropertyFetcher returns a GhPropertyFetcher for the given entity type
 func (_ ghEntityFetcher) EntityPropertyFetcher(entType minderv1.Entity) GhPropertyFetcher {
-	if entType == minderv1.Entity_ENTITY_REPOSITORIES {
+	// nolint:exhaustive
+	switch entType {
+	case minderv1.Entity_ENTITY_PULL_REQUESTS:
+		return NewPullRequestFetcher()
+	case minderv1.Entity_ENTITY_REPOSITORIES:
 		return NewRepositoryFetcher()
 	}
 
 	return nil
 }
 
-// RepoV1FromProperties creates a minderv1.Repository from a properties.Properties
-func RepoV1FromProperties(repoProperties *properties.Properties) (*minderv1.Repository, error) {
-	name, err := repoProperties.GetProperty(RepoPropertyName).AsString()
-	if err != nil {
-		return nil, fmt.Errorf("error fetching name property: %w", err)
+type propertyOrigin struct {
+	keys    []string
+	wrapper GhPropertyWrapper
+}
+
+type propertyFetcherBase struct {
+	propertyOrigins       []propertyOrigin
+	operationalProperties []string
+}
+
+var _ ghPropertyFetcherBase = propertyFetcherBase{}
+
+// WrapperForProperty returns the property wrapper for the given property key
+func (pfb propertyFetcherBase) WrapperForProperty(propertyKey string) GhPropertyWrapper {
+	for _, po := range pfb.propertyOrigins {
+		for _, k := range po.keys {
+			if k == propertyKey {
+				return po.wrapper
+			}
+		}
 	}
 
-	owner, err := repoProperties.GetProperty(RepoPropertyOwner).AsString()
-	if err != nil {
-		return nil, fmt.Errorf("error fetching owner property: %w", err)
-	}
+	return nil
+}
 
-	repoId, err := repoProperties.GetProperty(RepoPropertyId).AsInt64()
-	if err != nil {
-		return nil, fmt.Errorf("error fetching repo_id property: %w", err)
+// AllPropertyWrappers returns all property wrappers for the repository
+func (pfb propertyFetcherBase) AllPropertyWrappers() []GhPropertyWrapper {
+	wrappers := make([]GhPropertyWrapper, 0, len(pfb.propertyOrigins))
+	for _, po := range pfb.propertyOrigins {
+		wrappers = append(wrappers, po.wrapper)
 	}
+	return wrappers
+}
 
-	isPrivate, err := repoProperties.GetProperty(properties.RepoPropertyIsPrivate).AsBool()
-	if err != nil {
-		return nil, fmt.Errorf("error fetching is_private property: %w", err)
-	}
-
-	isFork, err := repoProperties.GetProperty(properties.RepoPropertyIsFork).AsBool()
-	if err != nil {
-		return nil, fmt.Errorf("error fetching is_fork property: %w", err)
-	}
-
-	pbRepo := &minderv1.Repository{
-		Name:          name,
-		Owner:         owner,
-		RepoId:        repoId,
-		HookId:        repoProperties.GetProperty(RepoPropertyHookId).GetInt64(),
-		HookUrl:       repoProperties.GetProperty(RepoPropertyHookUrl).GetString(),
-		DeployUrl:     repoProperties.GetProperty(RepoPropertyDeployURL).GetString(),
-		CloneUrl:      repoProperties.GetProperty(RepoPropertyCloneURL).GetString(),
-		HookType:      repoProperties.GetProperty(RepoPropertyHookType).GetString(),
-		HookName:      repoProperties.GetProperty(RepoPropertyHookName).GetString(),
-		HookUuid:      repoProperties.GetProperty(RepoPropertyHookUiid).GetString(),
-		IsPrivate:     isPrivate,
-		IsFork:        isFork,
-		DefaultBranch: repoProperties.GetProperty(RepoPropertyDefaultBranch).GetString(),
-		License:       repoProperties.GetProperty(RepoPropertyLicense).GetString(),
-	}
-
-	return pbRepo, nil
+// OperationalProperties returns the operational properties for the repository
+func (pfb propertyFetcherBase) OperationalProperties() []string {
+	return pfb.operationalProperties
 }
