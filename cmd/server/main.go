db823fe8f993b2a70195be50f98700bd718b131d
Michelangelo Mori <mmori@stacklok.com>
2024-09-16 12:16:13 +0200

    Profiling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,8 +4,24 @@
 // Package main provides the entrypoint for the minder server
 package main
 
-import "github.com/mindersec/minder/cmd/server/app"
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"runtime/pprof"
+
+	"github.com/mindersec/minder/cmd/server/app"
+)
 
 func main() {
+	f, err := os.Create("output.prof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	runtime.SetCPUProfileRate(1000)
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
 	app.Execute()
 }
