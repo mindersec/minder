e66a5789d58c489a4565d479848b954f8e031460
Jakub Hrozek <jakub@stacklok.com>
2024-10-07 13:15:28 +0200

    Remove DeleteEntityByName
    With multiple providers, we might have entities with the same name.
    Let's just use DeleteEntity which deletes by ID, forcing the API user to
    do a search before.

--- database/mock/fixtures/store.go
+++ database/mock/fixtures/store.go
@@ -152,9 +152,6 @@ func WithSuccessfulDeletePullRequest() func(*mockdb.MockStore) {
 		mockStore.EXPECT().
 			DeletePullRequest(gomock.Any(), gomock.Any()).
 			Return(nil)
-		mockStore.EXPECT().
-			DeleteEntityByName(gomock.Any(), gomock.Any()).
-			Return(nil)
 	}
 }
 
