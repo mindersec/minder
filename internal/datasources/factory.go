09d2c9258df6b4c5b996da3ae237741a7efd3126
Juan Antonio Osorio <ozz@stacklok.com>
2024-11-20 12:59:26 +0200

    Add builder function for data sources
    Signed-off-by: Juan Antonio Osorio <ozz@stacklok.com>

---
+++ internal/datasources/factory.go
@@ -0,0 +1,32 @@
+// SPDX-FileCopyrightText: Copyright 2023 The Minder Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package datasources implements data sources for Minder.
+package datasources
+
+import (
+	"fmt"
+
+	"github.com/mindersec/minder/internal/datasources/rest"
+	minderv1 "github.com/mindersec/minder/pkg/api/protobuf/go/minder/v1"
+	v1datasources "github.com/mindersec/minder/pkg/datasources/v1"
+)
+
+// BuildFromProtobuf is a factory function that builds a new data source based on the given
+// data source type.
+func BuildFromProtobuf(name string, ds *minderv1.DataSource) (v1datasources.DataSource, error) {
+	if ds == nil {
+		return nil, fmt.Errorf("data source is nil")
+	}
+
+	if ds.GetDriver() == nil {
+		return nil, fmt.Errorf("data source driver is nil")
+	}
+
+	switch ds.GetDriver().(type) {
+	case *minderv1.DataSource_Rest:
+		return rest.NewRestDataSource(name, ds.GetRest())
+	default:
+		return nil, fmt.Errorf("unknown data source type: %T", ds)
+	}
+}
