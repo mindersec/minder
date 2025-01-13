8d83735930fcfea2601093111738ebf58ca4b0db
Juan Antonio Osorio <ozz@stacklok.com>
2025-01-13 12:01:26 +0200

    Update cmd/dev/app/datasource/generate.go
    Co-authored-by: Radoslav Dimitrov <radoslav@stacklok.com>

--- cmd/dev/app/datasource/generate.go
+++ cmd/dev/app/datasource/generate.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2023 The Minder Authors
+// SPDX-FileCopyrightText: Copyright 2025 The Minder Authors
 // SPDX-License-Identifier: Apache-2.0
 
 package datasource
