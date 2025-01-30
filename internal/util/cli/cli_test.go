5f939c384e66fc1389f588e776fce0bfb371918b
Evan Anderson <evan@stacklok.com>
2025-01-29 23:56:15 -0800

    Fix lint complaint

--- internal/util/cli/cli_test.go
+++ internal/util/cli/cli_test.go
@@ -8,40 +8,8 @@ import (
 	"path/filepath"
 	"testing"
 
-	_ "github.com/lib/pq"
 	"github.com/mindersec/minder/internal/util/cli"
 )
-
-/*
-var (
-
-	// envLockXdgConfigHome is a mutex to ensure that all the tests that run os.SetEnv("XDG_CONFIG_HOME") need to be prevented from running at the same time as each other.
-	envLockXdgConfigHome = &sync.Mutex{}
-
-	// envLockXdgConfig is a mutex to ensure that all the tests that run os.SetEnv("MINDER_AUTH_TOKEN") need to be prevented from running at the same time as each other.
-	envLockMinderAuthToken = &sync.Mutex{}
-	//nolint:gosec // This is not a hardcoded credential
-	XdgConfigHomeEnvVar = "XDG_CONFIG_HOME"
-	//nolint:gosec // This is not a hardcoded credential
-	MinderAuthTokenEnvVar = "MINDER_AUTH_TOKEN"
-)
-
-// Based on tests, seemed to need one mutex per env var.
-func setEnvVar(t *testing.T, envLock *sync.Mutex, env string, value string) {
-	t.Helper() // Keep golangci-lint happy
-	envLock.Lock()
-	t.Cleanup(envLock.Unlock)
-
-	originalEnvVal := os.Getenv(env)
-	err := os.Setenv(env, value)
-	if err != nil {
-		t.Errorf("error setting %v: %v", env, err)
-	}
-
-	t.Cleanup(func() { _ = os.Setenv(env, originalEnvVal) })
-
-}
-*/
 
 // TestGetConfigDirPath tests the GetConfigDirPath function
 func TestGetConfigDirPath(t *testing.T) {
