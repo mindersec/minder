58cd726cd9c73e777e44956f6c6fae0fa7b07fa1
Evan Anderson <evan@stacklok.com>
2025-03-25 11:42:01 -0700

    Fix detected race in interna/providers/github/webhook by locking channel access

--- internal/util/testqueue/passthroughqueue.go
+++ internal/util/testqueue/passthroughqueue.go
@@ -5,6 +5,7 @@
 package testqueue
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/ThreeDotsLabs/watermill/message"
@@ -13,8 +14,9 @@ import (
 // PassthroughQueue is a queue that passes messages through.
 // It's only useful for testing.
 type PassthroughQueue struct {
-	ch chan *message.Message
-	t  *testing.T
+	ch     chan *message.Message
+	chLock sync.Mutex
+	t      *testing.T
 }
 
 // NewPassthroughQueue creates a new PassthroughQueue
@@ -29,18 +31,24 @@ func NewPassthroughQueue(t *testing.T) *PassthroughQueue {
 
 // GetQueue returns the queue
 func (q *PassthroughQueue) GetQueue() <-chan *message.Message {
+	q.chLock.Lock()
+	defer q.chLock.Unlock()
 	return q.ch
 }
 
 // Pass passes a message through the queue
 func (q *PassthroughQueue) Pass(msg *message.Message) error {
 	q.t.Logf("Passing message through queue: %s", msg.UUID)
+	q.chLock.Lock()
+	defer q.chLock.Unlock()
 	q.ch <- msg
 	return nil
 }
 
 // Close frees closes the channel used as queue.
 func (q *PassthroughQueue) Close() error {
+	q.chLock.Lock()
+	defer q.chLock.Unlock()
 	close(q.ch)
 	return nil
 }
