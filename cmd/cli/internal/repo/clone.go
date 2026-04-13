5f0550480f7dda7c23646b1926a8aebb0a6e9d90
Sachin Kumar <sachinkumar905846@gmail.com>
2026-04-13 21:17:14 +0530

    chore(cli): add license header to clone utility

--- cmd/cli/internal/repo/clone.go
+++ cmd/cli/internal/repo/clone.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2026 The Minder Authors
+// SPDX-License-Identifier: Apache-2.0
+
 // Package repo provides helpers to interact with git repositories.
 package repo
 
