6cb71e3a1bd7eebbd94c413361b6d4f42c7758a9
Sachin Kumar <sachinkumar905846@gmail.com>
2026-04-13 20:45:47 +0530

    feat(cli): add in-memory repository clone utility

---
+++ cmd/cli/internal/repo/clone.go
@@ -0,0 +1,18 @@
+// Package repo provides helpers to interact with git repositories.
+package repo
+
+import (
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/storage/memory"
+)
+
+// CloneInMemory clones a git repository into memory.
+//
+// This is a preparatory utility for future enhancements where
+// quickstart will dynamically load rule and profile catalogs
+// from a repository without touching disk.
+func CloneInMemory(url string) (*git.Repository, error) {
+	return git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL: url,
+	})
+}
