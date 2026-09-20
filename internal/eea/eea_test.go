ebd3526e1a5d868fd7163049f3d2d4dd8cd45fbc
shreyaabaranwal <shreyabaranwal229@gmail.com>
2026-09-21 02:34:52 +0530

    Fix race condition in TestAggregator/TestFlushAll
    Synchronize background goroutines with the main test goroutine using
    buffered channels instead of calling t.Log/assert/require from within
    them. Verified with 5 repeated -race runs, all clean.
    
    Fixes #6794

--- internal/eea/eea_test.go
+++ internal/eea/eea_test.go
@@ -90,12 +90,15 @@ func TestAggregator(t *testing.T) {
 	// This tests that flushing sends messages to the executor engine
 	evt.Register(constants.TopicQueueEntityEvaluate, flushedMessages.Add, aggr.AggregateMiddleware)
 
+	t.Log("Running eventer")
+	runErr := make(chan error, 1)
 	go func() {
-		t.Log("Running eventer")
-		err := evt.Run(ctx)
-		assert.NoError(t, err, "expected no error when running eventer")
+		runErr <- evt.Run(ctx)
 	}()
-	defer evt.Close()
+	defer func() {
+		require.NoError(t, evt.Close())
+		require.NoError(t, <-runErr)
+	}()
 
 	inf := entities.NewEntityInfoWrapper().
 		WithRepository(&minderv1.Repository{}).
@@ -355,10 +358,9 @@ func TestFlushAll(t *testing.T) {
 			flushedMessages := newTestPubSub()
 			evt.Register(constants.TopicQueueEntityEvaluate, flushedMessages.Add)
 
+			runErr := make(chan error, 1)
 			go func() {
-				t.Log("Running eventer")
-				err := evt.Run(ctx)
-				assert.NoError(t, err, "expected no error when running eventer")
+				runErr <- evt.Run(ctx)
 			}()
 
 			<-evt.Running()
@@ -372,15 +374,19 @@ func TestFlushAll(t *testing.T) {
 			tt.mockDBSetup(ctx, mockStore)
 			tt.mockPropSvcSetup(propsvc)
 
+			flushErr := make(chan error, 1)
 			go func() {
-				t.Log("Flushing all")
-				require.NoError(t, aggr.FlushAll(ctx), "expected no error")
+				flushErr <- aggr.FlushAll(ctx)
 			}()
 
 			t.Log("Waiting for flush")
 			flushedMessages.Wait()
+			require.NoError(t, <-flushErr)
 
 			assert.Equal(t, int32(1), flushedMessages.count.Load(), "expected one message")
+
+			cancel()
+			require.NoError(t, <-runErr)
 		})
 	}
 }
